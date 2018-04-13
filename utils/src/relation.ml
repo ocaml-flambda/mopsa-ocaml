25e5579bacfb16c766d20ed5d9bdca910157675a
Antoine Miné <antoine.mine@lip6.fr>
2018-04-13 23:10:06 +0200

    [utils] relations, based on maps to sets (i.e., multimaps)

---
+++ utils/src/relation.ml
@@ -0,0 +1,319 @@
+
+open RelationSig
+
+module Make(Dom: OrderedType)(CoDom: OrderedType) = (struct
+
+  module CoDomSet = SetExt.Make(CoDom)
+  module DomMap = MapExt.Make(Dom)
+    
+  type t = CoDomSet.t DomMap.t
+  (** A relation is a map from the domain to the power-set of the codomain.
+      The image f(x) of an element is never the empty set.
+   *)
+
+  type dom = Dom.t
+  type codom = CoDom.t
+  type codom_set = CoDomSet.t
+  type binding = dom * codom
+            
+  let empty =
+    DomMap.empty
+            
+  let image x r =
+    try DomMap.find x r with Not_found -> CoDomSet.empty
+
+  let set_image x ys r =
+    if CoDomSet.is_empty ys then DomMap.remove x r
+    else DomMap.add x ys r
+
+  let is_image_empty x r =
+    not (DomMap.mem x r)
+
+  let is_empty r =
+    DomMap.is_empty r
+
+
+  let singleton x y =
+    DomMap.singleton x (CoDomSet.singleton y)
+    
+  let add x y r =
+    DomMap.add x (CoDomSet.add y (image x r)) r
+
+  let add_set x ys r =
+    set_image x (CoDomSet.union ys (image x r)) r
+
+  let remove x y r =
+    set_image x (CoDomSet.remove y (image x r)) r
+    
+  let remove_set x ys r =
+    set_image x (CoDomSet.diff (image x r) ys) r
+
+  let remove_image x r =
+    DomMap.remove x r
+
+  let mem x y r =
+    CoDomSet.mem y (image x r)
+
+    
+  let of_list l =
+    List.fold_left (fun r (x,y) -> add x y r) empty l
+
+  let min_binding r =
+    let x,ys = DomMap.min_binding r in
+    x, CoDomSet.min_elt ys
+    
+  let max_binding r =
+    let x,ys = DomMap.max_binding r in
+    x, CoDomSet.max_elt ys
+
+  let choose r =
+    let x,ys = DomMap.choose r in
+    x, CoDomSet.choose ys
+       
+  let cardinal r =
+    DomMap.fold (fun _ i r -> r + CoDomSet.cardinal i) r 0
+
+
+    
+  let iter f r =
+    DomMap.iter (fun x i -> CoDomSet.iter (fun y -> f x y) i) r
+
+  let fold f r acc =
+    DomMap.fold
+      (fun x i acc -> CoDomSet.fold (fun y acc -> f x y acc) i acc)
+      r acc
+
+  let bindings r =
+    List.rev (fold (fun x y l -> (x,y)::l) r [])
+
+  let map f r =
+    fold
+      (fun x y acc -> let x',y' = f x y in add x' y' acc)
+      r empty
+    
+  let domain_map f r =
+    DomMap.fold
+      (fun x i r -> add_set (f x) i r)
+      r DomMap.empty
+
+  let codomain_map f r =
+    DomMap.map (CoDomSet.map f) r
+
+  let for_all f r =
+    DomMap.for_all (fun x i -> CoDomSet.for_all (fun y -> f x y) i) r
+    
+  let exists f r =
+    DomMap.exists (fun x i -> CoDomSet.exists (fun y -> f x y) i) r
+
+  let filter f r =
+    DomMap.fold
+      (fun x i r -> set_image x (CoDomSet.filter (fun y -> f x y) i) r) r r
+
+
+  (* binary operations *)
+
+
+      
+  let compare r1 r2 =
+    DomMap.compare CoDomSet.compare r1 r2
+    
+  let equal r1 r2 =
+    DomMap.equal CoDomSet.equal r1 r2
+
+  let subset r1 r2 =
+    DomMap.for_all2zo
+      (fun _ _ -> false)
+      (fun _ _ -> true)
+      (fun _ s1 s2 -> CoDomSet.subset s1 s2)
+      r1 r2
+
+  let union r1 r2 =
+    DomMap.map2zo
+      (fun _ ys -> ys)
+      (fun _ ys -> ys)
+      (fun _ ys1 ys2 -> CoDomSet.union ys1 ys2)
+      r1 r2
+
+  let inter r1 r2 =
+    (* start from r1
+       - remove x's image if x is only in r1
+       - nothing if x is only in r2 (as it is not in r1)
+       - update x's image if both in r1 and r2
+     *)
+    DomMap.fold2zo
+      (fun x _ r -> remove_image x r)
+      (fun _ _ r -> r)
+      (fun x ys1 ys2 r -> set_image x (CoDomSet.inter ys1 ys2) r)
+      r1 r2 r1
+
+  let diff r1 r2 =
+    (* start from r1
+       - nothing if x is only in r1
+       - nothing if x is only in r2 (as it is not in r1)
+       - update x's image if both in r1 and r2
+     *)
+    DomMap.fold2o
+      (fun _ _ r -> r)
+      (fun _ _ r -> r)
+      (fun x ys1 ys2 r -> set_image x (CoDomSet.diff ys1 ys2) r)
+      r1 r2 r1
+
+    
+  let iter2 f1 f2 f r1 r2 =
+    DomMap.iter2o
+    (fun x -> CoDomSet.iter (f1 x))
+    (fun x -> CoDomSet.iter (f2 x))
+    (fun x -> CoDomSet.iter2 (f1 x) (f2 x) (f x))
+    r1 r2
+    
+  let iter2_diff f1 f2 r1 r2 =
+    DomMap.iter2o
+    (fun x -> CoDomSet.iter (f1 x))
+    (fun x -> CoDomSet.iter (f2 x))
+    (fun x -> CoDomSet.iter2_diff (f1 x) (f2 x))
+    r1 r2
+
+
+  let fold2 f1 f2 f r1 r2 acc =
+    DomMap.fold2o
+    (fun x -> CoDomSet.fold (f1 x))
+    (fun x -> CoDomSet.fold (f2 x))
+    (fun x -> CoDomSet.fold2 (f1 x) (f2 x) (f x))
+    r1 r2 acc
+    
+  let fold2_diff f1 f2 r1 r2 =
+    DomMap.fold2zo
+    (fun x -> CoDomSet.fold (f1 x))
+    (fun x -> CoDomSet.fold (f2 x))
+    (fun x -> CoDomSet.fold2_diff (f1 x) (f2 x))
+    r1 r2
+
+
+  let for_all2 f1 f2 f r1 r2 =
+    DomMap.for_all2o
+    (fun x -> CoDomSet.for_all (f1 x))
+    (fun x -> CoDomSet.for_all (f2 x))
+    (fun x -> CoDomSet.for_all2 (f1 x) (f2 x) (f x))
+    r1 r2
+    
+  let for_all2_diff f1 f2 r1 r2 =
+    DomMap.for_all2o
+    (fun x -> CoDomSet.for_all (f1 x))
+    (fun x -> CoDomSet.for_all (f2 x))
+    (fun x -> CoDomSet.for_all2_diff (f1 x) (f2 x))
+    r1 r2
+
+
+  let exists2 f1 f2 f r1 r2 =
+    DomMap.exists2o
+    (fun x -> CoDomSet.exists (f1 x))
+    (fun x -> CoDomSet.exists (f2 x))
+    (fun x -> CoDomSet.exists2 (f1 x) (f2 x) (f x))
+    r1 r2
+    
+  let exists2_diff f1 f2 r1 r2 =
+    DomMap.exists2o
+    (fun x -> CoDomSet.exists (f1 x))
+    (fun x -> CoDomSet.exists (f2 x))
+    (fun x -> CoDomSet.exists2_diff (f1 x) (f2 x))
+    r1 r2
+
+
+
+  (* domain operations *)
+    
+  let iter_domain f r =
+    DomMap.iter f r
+
+  let fold_domain f r acc =
+    DomMap.fold f r acc
+
+  let map_domain f r =
+    DomMap.fold
+      (fun x i r -> set_image x (f x i) r)
+      r DomMap.empty
+
+  let for_all_domain f r =
+    DomMap.for_all f r
+
+  let exists_domain f r =
+    DomMap.exists f r
+
+  let filter_domain f r =
+    DomMap.filter f r
+
+  let min_domain r =
+    fst (DomMap.min_binding r)
+
+  let max_domain r =
+    fst (DomMap.max_binding r)
+
+  let choose_domain r =
+    fst (DomMap.choose r)
+
+  let cardinal_domain r =
+    DomMap.cardinal r
+
+  let elements_domain r =
+    List.rev (DomMap.fold (fun x _ l -> x::l) r [])
+    
+
+  (* printing *)
+
+    
+  type relation_printer = {
+      print_empty: string;
+      print_begin: string;
+      print_open: string;
+      print_comma: string;
+      print_close: string;
+      print_sep: string;
+      print_end: string;
+    }
+                   
+  let printer_default = {
+      print_empty="{}";
+      print_begin="{";
+      print_open="(";
+      print_comma=",";
+      print_close=")";
+      print_sep=";";
+      print_end="}";
+    }
+                      
+  let print_gen o printer dom codom ch s =
+    if is_empty s then o ch printer.print_empty else (
+      let first = ref true in
+      o ch printer.print_begin;
+      iter
+        (fun x y ->
+          if !first then first := false else o ch printer.print_sep;
+          o ch printer.print_open;
+          dom ch x;
+          o ch printer.print_comma;
+          codom ch y;
+          o ch printer.print_close;
+        ) s;
+      o ch printer.print_end
+    )
+  (* internal printing helper *)
+    
+  let print printer dom codom ch l = print_gen output_string printer dom codom ch l
+  let bprint printer dom codom ch l = print_gen Buffer.add_string printer dom codom ch l
+  let fprint printer dom codom ch l = print_gen Format.pp_print_string printer dom codom ch l
+                              
+  let to_string printer dom codom l =
+    let b = Buffer.create 10 in
+    print_gen (fun () s -> Buffer.add_string b s) printer
+              (fun () k -> dom k)
+              (fun () k -> codom k)
+              () l;
+    Buffer.contents b
+
+    
+ end: S with
+      type dom = Dom.t and
+      type codom = CoDom.t and
+      module CoDomSet = SetExt.Make(CoDom))
+                                                  
+                                                   
---
+++ utils/src/relationSig.ml
@@ -0,0 +1,374 @@
+
+module type OrderedType = MapExtSig.OrderedType
+
+module type S = sig
+
+  
+  (** {2 Types} *)
+
+  type t
+  (** Represents a relation between a domain and a codomain.
+      An element of [t] can be seen as a set of bindings, i.e.,
+      a subset of [dom] * [codom], or as a function from [dom] to 
+      subsets of [codom] (i.e., a multi-map).
+      Relations are imutable, purely functional data-structures.
+
+      Internally, the relation is represented as a map to sets.
+      As a consequence, it is easy to get the post-image of an element of 
+      the domain, but hard to get the pre-image of an element of the 
+      codomain (use [InvRelation] if this is needed).
+      We use Maps and Sets, so that it is required to provide
+      [OrderedType] structures for the domain and codomain.
+   *)
+
+     
+     
+  type dom
+  (** An element of the domain. *)
+
+  type codom
+  (** An element of the codomain. *)
+             
+  module CoDomSet : SetExtSig.S
+  (** Data-type for sets of codomain elements. *)
+
+  type codom_set = CoDomSet.t
+  (** A set of elements of the codomain. *)
+                 
+  type binding = dom * codom
+  (** A binding. *)
+            
+
+  (** {2 Construction and update} *)
+
+  val empty: t
+  (** The empty relation. *)
+    
+  val image: dom -> t -> codom_set
+  (** [image x r] is the set of elements associated to [x] in [r]. 
+   *)
+
+  val set_image: dom -> codom_set -> t -> t
+  (** [set_image x ys r] is a new relation where the image of [x]
+      has been updated to be [ys].
+   *)
+
+  val is_image_empty: dom -> t -> bool
+  (** [is_image_empty x r] returns true if there is no element associated 
+      to [x] in [r]
+   *)
+
+  val is_empty: t -> bool
+  (** Whether the relation is empty. *)
+
+  val singleton: dom -> codom -> t
+  (** [singleton x y] is the relation with a unique binding ([x],[y]). 
+   *)
+    
+  val add: dom -> codom -> t -> t
+  (** [add x y r] returns [r] with a new binding ([x],[y]) added. 
+      Previous bindings to [x] are preserved.
+   *)
+
+  val add_set: dom -> codom_set -> t -> t
+  (** [add_set x ys r] returns [r] with all the bindings ([x],[y]) for
+      [y] in the set [ys] added. 
+      Previous bindings to [x] are preserved.
+   *)
+
+  val remove: dom -> codom -> t -> t
+  (** [remove x y r] returns [r] with the binding ([x],[y]) removed,
+      if it exists.
+      Other bindings to [x] are preserved.
+   *)
+
+  val remove_set: dom -> codom_set -> t -> t
+  (** [remove_set x ys r] returns [r] with a all bindings ([x],[y]) for
+      [y] in the set [ys] removed.
+      Other bindings to [x] are preserved.
+   *)
+
+  val remove_image: dom -> t -> t
+  (** [remove_image x r] returns [r] with all bindings to [x] removed.
+   *)
+
+  val mem: dom -> codom -> t -> bool
+  (** [mem x y r] returns [true] if the binding ([x],[y]) is present in [r].
+   *)
+
+  val of_list: binding list -> t
+  (** [of_list l] returns a relation constructed from the list of bindings. *)
+
+  val bindings: t -> binding list
+  (** [bindings r] lists the bindings in the relation. 
+   *)
+
+  val min_binding: t -> binding
+  (** [min_binding r] returns the smallest binding in [r], for the 
+      lexicographic order of domain and codomain.
+      Raises [Not_found] if the relation is empty.
+   *)
+     
+  val max_binding: t -> binding
+  (** [max_binding r] returns the largest binding in [r], for the 
+      lexicographic order of domain and codomain.
+      Raises [Not_found] if the relation is empty.
+   *)
+     
+  val choose: t -> binding
+  (** [choose r] returns an arbitrary binding in [r].
+      Raises [Not_found] if the relation is empty.
+   *)
+     
+  val cardinal: t -> int
+  (** [cardinal r] returns the number of bindings in [r]. *)
+
+    
+  (** {2 Global operations} *)
+
+  val iter: (dom -> codom -> unit) -> t -> unit
+  (** [iter f r] applies [f x y] to every binding ([x],[y]) of [r]. 
+      The bindings are considered in increasing order for the lexicographic
+      order sorting through dom first, and then codom.
+   *)
+    
+    
+  val fold: (dom -> codom -> 'a -> 'a) -> t -> 'a -> 'a
+  (** [fold f r acc] folds [acc] through every binding ([x],[y]) in [r] through [f]. 
+      The bindings are considered in increasing order for the lexicographic
+      order sorting through dom first, and then codom.
+   *)
+
+  val map: (dom -> codom -> binding) -> t -> t
+  (** [map f r] returns a relation where every binding ([x],[y]) is replaced 
+      with ([x'],[y'])=[f x y]. 
+      The bindings are considered in increasing order for the lexicographic
+      order sorting through dom first, and then codom.
+   *)
+
+  val domain_map: (dom -> dom) -> t -> t
+  (** [domain_map f r] returns a relation where every binding ([x],[y]) is 
+      replaced with ([f x],[y]). 
+      The bindings are considered in increasing order for the lexicographic
+      order sorting through dom first, and then codom.
+   *)
+
+  val codomain_map: (codom -> codom) -> t -> t
+  (** [codomain_map f r] returns a relation where every binding ([x],[y]) 
+      is replaced with ([x],[f y]). 
+      The bindings are considered in increasing order for the lexicographic
+      order sorting through dom first, and then codom.
+   *)
+
+  val for_all: (dom -> codom -> bool) -> t -> bool
+  (** [for_all f r] returns true if [f x y] is true for every binding 
+      ([x],[y]) in [r]. 
+      The bindings are considered in increasing order for the lexicographic
+      order sorting through dom first, and then codom.
+   *)
+
+  val exists: (dom -> codom -> bool) -> t -> bool
+  (** [exists f r] returns true if [f x y] is true for at leat one  
+      binding ([x],[y]) in [r]. 
+      The bindings are considered in increasing order for the lexicographic
+      order sorting through dom first, and then codom.
+   *)
+
+  val filter: (dom -> codom -> bool) -> t -> t
+  (** [filter f r] returns a relation with only the bindings ([x],[y]) 
+      from [r] where [f x y] is true. 
+   *)
+
+
+
+  (** {2 Set operations} *)
+
+  val compare: t -> t -> int
+  (** Total ordering function that returns -1, 0 or 1. *)
+    
+  val equal: t -> t -> bool
+  (** Whether the two relations have the exact same set of bindings. *)
+    
+  val subset: t -> t -> bool
+  (** [subset r1 r2] returns whether the set of bindings in [s1] is
+      included in the set of bindings in [s2].
+   *)
+
+  val union: t -> t -> t
+  (** [union r s] is the union of the bindings of [r] and [s]. *)
+    
+  val inter: t -> t -> t
+  (** [inter r s] is the intersection of the bindings of [r] and [s]. *)
+    
+  val diff: t -> t -> t
+  (** [diff r s] is the set difference of the bindings of [r] and [s]. *)
+
+
+
+  (** {2 Binary operations} *)
+
+  val iter2: (dom -> codom -> unit) -> (dom -> codom -> unit) -> (dom -> codom -> unit) -> t -> t -> unit
+  (** [iter2 f1 f2 f r1 r2] applies [f1] to the bindings only in [r1],
+      [f2] to the bindings only in [r2], and [f] to the bindings in both
+      [r1] and [r2].
+      The bindings are considered in increasing lexicographic order.
+   *)
+    
+  val fold2: (dom -> codom -> 'a -> 'a) -> (dom -> codom -> 'a -> 'a) -> (dom -> codom -> 'a -> 'a) -> t -> t -> 'a -> 'a
+  (** [fold2 f1 f2 f r1 r2] applies [f1] to the bindings only in [r1],
+      [f2] to the bindings only in [r2], and [f] to the bindings in both
+      [r1] and [r2].
+      The bindings are considered in increasing lexicographic order.
+   *)
+
+  val for_all2: (dom -> codom -> bool) -> (dom -> codom -> bool) -> (dom -> codom -> bool) -> t -> t -> bool
+  (** [for_all2 f1 f2 f r1 r2] is true if [f1] is true on all the bindings
+      only in [r1], [f2] is true on all the bindings only in [r2], and [f] 
+      is true on all the bindings both in [r1] and [r2].
+      The bindings are considered in increasing lexicographic order.
+   *)
+ 
+  val exists2: (dom -> codom -> bool) -> (dom -> codom -> bool) -> (dom -> codom -> bool) -> t -> t -> bool
+  (** [exists2 f1 f2 f r1 r2] is true if [f1] is true on one binding
+      only in [r1] or [f2] is true on one binding only in [r2], or [f] 
+      is true on one binding both in [r1] and [r2].
+      The bindings are considered in increasing lexicographic order.
+   *)
+
+
+    
+  val iter2_diff: (dom -> codom -> unit) -> (dom -> codom -> unit) -> t -> t -> unit
+  (** [iter2_diff f1 f2 r1 r2] applies [f1] to the bindings only in [r1]
+      and [f2] to the bindings only in [r2].
+      The bindings both in [r1] and [r2] are ignored.
+      The bindings are considered in increasing lexicographic order.
+      It is equivalent to calling [iter2] with [f = fun x y -> ()], 
+      but more efficient. 
+   *)
+
+
+  val fold2_diff: (dom -> codom -> 'a -> 'a) -> (dom -> codom -> 'a -> 'a) -> t -> t -> 'a -> 'a
+  (** [fold2_diff f1 f2 r1 r2] applies [f1] to the bindings only in [r1]
+      and [f2] to the bindings only in [r2].
+      The bindings both in [r1] and [r2] are ignored.
+      The bindings are considered in increasing lexicographic order.
+      It is equivalent to calling [fold2] with [f = fun x y acc -> acc], 
+      but more efficient. 
+   *)
+
+  val for_all2_diff: (dom -> codom -> bool) -> (dom -> codom -> bool) -> t -> t -> bool
+  (** [for_all2_diff f1 f2 f r1 r2] is true if [f1] is true on all the bindings
+      only in [r1] and [f2] is true on all the bindings only in [r2].
+      The bindings both in [r1] and [r2] are ignored.
+      The bindings are considered in increasing lexicographic order.
+      It is equivalent to calling [for_all2] with [f = fun x y -> true], but more efficient. 
+     *)
+    
+  val exists2_diff: (dom -> codom -> bool) -> (dom -> codom -> bool) -> t -> t -> bool
+  (** [exists2_diff f1 f2 f r1 r2] is true if [f1] is true on one binding
+      only in [r1] or [f2] is true on one binding only in [r2].
+      The bindings both in [r1] and [r2] are ignored.
+      The bindings are considered in increasing lexicographic order.
+      It is equivalent to calling [exists2] with [f = fun x y -> false], but more efficient. 
+     *)
+    
+    
+    
+  (** {2 Multi-map operations} *)
+
+  (** These functions consider the relation as a map from domain elements
+      to codomain sets.
+   *)
+    
+  val iter_domain: (dom -> codom_set -> unit) -> t -> unit
+  (** [iter_domain f r] applies [f x ys] for every domain element [x] and
+      its image set [ys] in [r].
+      The domain elements are considered in increasing order.
+   *)
+
+  val fold_domain: (dom -> codom_set -> 'a -> 'a) -> t -> 'a -> 'a
+  (** [fold_domain f r acc] applies [f x ys acc] for every domain element 
+      [x] and its image set [ys] in [r].
+      The domain elements are considered in increasing order.
+   *)
+
+  val map_domain: (dom -> codom_set -> codom_set) -> t -> t
+  (** [map_domain f r] returns new relation where the image set [ys] of
+      [x] in [r] is replaced with [f x ys]. 
+      The domain elements are considered in increasing order.
+   *)
+
+  val for_all_domain: (dom -> codom_set -> bool) -> t -> bool
+  (** [for_all_domain f r] returns true if [f x ys] us true for every 
+      domain element [x] and its image set [ys] in [r].
+      The domain elements are considered in increasing order.
+   *)
+
+  val exists_domain: (dom -> codom_set -> bool) -> t -> bool
+  (** [exists_domain f r] returns true if [f x ys] us true for at least
+      one domain element [x] and its image set [ys] in [r].
+      The domain elements are considered in increasing order.
+   *)
+
+  val filter_domain: (dom -> codom_set -> bool) -> t -> t
+  (** [for_all_domain f r] returns a new relation restricted to the
+      a domain element [x] with its image [ys] from [r] such that
+      [f x ys] is true.
+   *)
+
+  val min_domain: t -> dom
+  (** [min_domain r] returns the smallest domain element in [r].
+      Raises [Not_found] if the relation is empty.
+   *)
+    
+  val max_domain: t -> dom
+  (** [max_domain r] returns the greatest domain element in [r]. 
+      Raises [Not_found] if the relation is empty.
+   *)
+    
+  val choose_domain: t -> dom
+  (** [choose_domain r] returns any domain element in [r]. 
+      Raises [Not_found] if the relation is empty.
+   *)
+
+  val cardinal_domain: t -> int
+  (** [cardinal r] returns the number of distinct domain elements used in [r]. *)
+
+  val elements_domain: t -> dom list
+  (** [elemets_domain r] returns the list of domain elements used in [r].
+      The elements are returned in increasing order.
+   *)
+
+    
+
+  (** {2 Printing} *)
+                                                
+  type relation_printer = {
+      print_empty: string; (** Special text for empty relations *)
+      print_begin: string; (** Text before the first binding *)
+      print_open: string;  (** Text before a domain element *)
+      print_comma: string; (** Text between a domain and a codomain element *)
+      print_close: string; (** Text after a codomain element *)
+      print_sep: string;   (** Text between two bindings *)
+      print_end: string;   (** Text after the last binding *)
+    }
+  (** Tells how to print a relation. *)
+
+  val printer_default: relation_printer
+  (** Print as set: {(dom1,codom1),...,(domN,codomN)} *)
+    
+  val to_string: relation_printer -> (dom -> string) -> (codom -> string) -> t -> string
+  (** String representation. *)
+    
+  val print: relation_printer -> (out_channel -> dom -> unit) -> (out_channel -> codom -> unit) -> out_channel -> t -> unit
+  (** Prints to an output_channel (for Printf.(f)printf). *)
+    
+  val fprint: relation_printer -> (Format.formatter -> dom -> unit) -> (Format.formatter -> codom -> unit) -> Format.formatter -> t -> unit
+  (** Prints to a formatter (for Format.(f)printf). *)
+    
+  val bprint: relation_printer -> (Buffer.t -> dom -> unit) -> (Buffer.t -> codom -> unit) -> Buffer.t -> t -> unit
+  (** Prints to a string buffer (for Printf.bprintf). *)                     
+
+end
+                                                  
+  
