4297981ccbddcfc00765607c029a6e4bbaaa9c8c
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-05-23 11:09:03 +0200

    [python] add unit tests for exceptions

--- analyzer/tests/python/exception_tests.py
+++ analyzer/tests/python/exception_tests.py
@@ -1,6 +1,6 @@
 import mopsa
 
-def test_raise_caught_with_same_exception():
+def test_catching_raised_exception():
     x = 1
     try:
         x = 2
@@ -10,13 +10,41 @@ def test_raise_caught_with_same_exception():
     mopsa.assert_safe()
     mopsa.assert_equal(x, 3)
 
+def test_catching_superclass_exception():
+    x = 0
+    y = None
+    try:
+        if x == 0:
+            raise ZeroDivisionError
+        else:
+            y = 10/x
+    except Exception:
+        y = 0
+    mopsa.assert_safe()
+    mopsa.assert_equal(y, 0)
+
+def test_catching_wrong_exception():
+    x = 0
+    try:
+        raise RuntimeError
+    except ZeroDivisionError:
+        y = 0
+    mopsa.assert_exception(RuntimeError)
+
+def test_default_except():
+    x = 0
+    y = None
+    try:
+        raise RuntimeError
+    except:
+        y = 0
+    mopsa.assert_safe()
+    mopsa.assert_equal(y, 0)
+
 def test_unreachable_test():
     x = 1
     if x > 2:
-        try:
-            raise 2
-        except 3:
-            pass
+        raise 2
     else:
         x = 10
     mopsa.assert_safe()
