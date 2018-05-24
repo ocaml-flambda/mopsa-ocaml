b641f20d0b6881ec9954891c42954189d518db38
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-05-24 16:59:18 +0200

    [*] fix minor bugs

--- analyzer/src/framework/ast.ml
+++ analyzer/src/framework/ast.ml
@@ -209,7 +209,7 @@ let compare_var v1 v2 =
   ]
 
 let mkv ?(vtyp = T_any) ?(vkind = V_orig) ?(vuid=0) vname =
-  {vname; vuid = 0; vtyp; vkind}
+  {vname; vuid; vtyp; vkind}
 
 let tmp_counter = ref 100
 
