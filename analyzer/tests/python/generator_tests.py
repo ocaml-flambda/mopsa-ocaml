6134cb5e7a53153ab7d460098185b67ae6dff5ce
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-05-31 18:39:32 +0200

    [python] add some unit tests for generators

---
+++ analyzer/tests/python/generator_tests.py
@@ -0,0 +1,60 @@
+import mopsa
+
+def test_single_yield():
+    def gen():
+        yield 1
+
+    g = gen()
+    x = next(g)
+    mopsa.assert_equal(x, 1)
+
+def test_two_yields():
+    def gen():
+        yield 1
+        yield 2
+    g = gen()
+    x = next(g)
+    x = next(g)
+    mopsa.assert_equal(x, 2)
+
+def test_argument():
+    def gen(x):
+        yield x + 1
+        yield x + 2
+    g = gen(10)
+    x = next(g)
+    x = next(g)
+    mopsa.assert_equal(x, 12)
+
+def test_stop_iteration_when_terminated():
+    def gen():
+        yield 1
+    g = gen()
+    x = next(g)
+    mopsa.assert_safe()
+    x = next(g)
+    mopsa.assert_exception(StopIteration)
+
+def test_auto_stop_iteration_when_terminated():
+    def gen():
+        yield 1
+    g = gen()
+    try:
+        x = next(g)
+        x = next(g)
+    except:
+        pass
+    mopsa.assert_safe()
+    x = next(g)
+    mopsa.assert_exception(StopIteration)
+
+def test_loop():
+    def gen():
+        i = 0
+        while i < 10:
+            yield i
+            i = i + 1
+    g = gen()
+    for x in g:
+        pass
+    mopsa.assert_equal(x, 9)
