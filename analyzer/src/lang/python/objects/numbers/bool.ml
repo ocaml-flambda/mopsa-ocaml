f7916ddee4639d77aa171f38664ef8f09e8d65ac
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-06-08 11:50:08 +0200

    [python] add bool.__new__

--- analyzer/src/lang/python/objects/numbers/bool.ml
+++ analyzer/src/lang/python/objects/numbers/bool.ml
@@ -35,10 +35,55 @@ module Domain= struct
       oeval_singleton (Some (mk_py_false range), flow, [])
 
     (* 𝔼⟦ bool.__new__(cls, arg) ⟧ *)
-    | E_py_call({ekind = E_py_object ({addr_kind = A_py_function (F_builtin "int.__bool__")}, _)}, cls :: args, []) ->
-      assert false
+    | E_py_call({ekind = E_py_object ({addr_kind = A_py_function (F_builtin "bool.__new__")}, _)}, cls :: args, []) ->
+      eval_list args (man.eval ctx) flow |>
+      eval_compose (fun el flow ->
+          match el with
+          | [] -> oeval_singleton (Some (mk_py_false range), flow, [])
+
+          | [arg] -> new_bool_of_object man ctx arg range flow
+
+          | _ ->
+            let flow = man.exec ctx (Utils.mk_builtin_raise "TypeError" range) flow in
+            oeval_singleton (None, flow, [])
+
+        )
 
     | _ -> None
+
+  and new_bool_of_object man ctx arg range flow =
+    let obj = object_of_expr arg in
+    let cls = class_of_object obj in
+    Universal.Utils.assume_to_eval
+      (Utils.mk_object_hasattr cls "__bool__" range)
+      (fun true_flow ->
+         let exp = mk_py_call (mk_py_object_attr cls "__bool__" range) [arg] range in
+         man.eval ctx exp flow |>
+         eval_compose (fun ret flow ->
+             let o = object_of_expr ret in
+             if Addr.isinstance o (Addr.find_builtin "bool") then
+               oeval_singleton (Some exp, flow, [])
+             else
+               let flow = man.exec ctx (Utils.mk_builtin_raise "TypeError" range) flow in
+               oeval_singleton (None, flow, [])
+           )
+      )
+      (fun false_flow ->
+         Universal.Utils.assume_to_eval
+           (Utils.mk_object_hasattr cls "__len__" range)
+           (fun true_flow ->
+              Universal.Utils.assume_to_eval
+                (mk_binop (mk_py_call (mk_py_object_attr cls "__len__" range) [arg] range) O_eq (mk_py_zero range) range)
+                (fun true_flow -> oeval_singleton (Some (mk_py_false range), true_flow, []))
+                (fun false_flow -> oeval_singleton (Some (mk_py_true range), false_flow, []))
+                man ctx true_flow ()
+           )
+           (fun false_flow ->
+              oeval_singleton (Some (mk_py_true range), false_flow, [])
+           )
+           man ctx false_flow ()
+      )
+      man ctx flow ()
 
 
   let init man ctx prog flow = ctx, flow
