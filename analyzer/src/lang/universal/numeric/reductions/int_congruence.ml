b947049abeb8b73d8c226b44ccd3eebe91f9f11f
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-06-17 00:57:21 +0200

    [universal] add interval-congruence reduction

---
+++ analyzer/src/lang/universal/numeric/reductions/int_congruence.ml
@@ -0,0 +1,55 @@
+(****************************************************************************)
+(*                   Copyright (C) 2017 The MOPSA Project                   *)
+(*                                                                          *)
+(*   This program is free software: you can redistribute it and/or modify   *)
+(*   it under the terms of the CeCILL license V2.1.                         *)
+(*                                                                          *)
+(****************************************************************************)
+
+(** Reduced product of integer intervals and congruences. *)
+
+open Framework.Essentials
+open Framework.Domains.Reductions.Value_reduction
+open Ast
+
+let name = "universal.numeric.reductions.int_congruence"
+let debug fmt = Debug.debug ~channel:name fmt
+
+module I = Values.Int.Value
+module C = Values.Congruence.Value
+
+type _ key +=
+  | Int : I.t key
+  | Cgr : C.t key
+
+module Reduction : REDUCTION =
+struct
+
+  let reduce man v =
+    try
+      let itv = Bot.bot_to_exn @@ man.get Int v in
+      let cgr = Bot.bot_to_exn @@ man.get Cgr v in
+      let reduction = Congruences.IntCong.meet_inter cgr itv in
+      let cgr, itv = Bot.bot_to_exn reduction in
+      man.set Cgr (Bot.Nb cgr) v |>
+      man.set Int (Bot.Nb itv)
+    with Bot.Found_BOT ->
+      man.bottom
+
+end
+
+
+let () =
+  register_reduction {
+    name;
+    pool = Pool.[Value (Int, (module I)); Value (Cgr, (module C))];
+    eq = (
+      let f : type a b. a key -> b key -> (a, b) Pool.eq option = fun k1 k2 ->
+        match k1, k2 with
+        | Int, Int -> Some Eq
+        | Cgr, Cgr -> Some Eq
+        | _ -> None
+      in
+      f
+    )
+  } (module Reduction)
