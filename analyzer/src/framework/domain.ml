4b9c0e373613f537173b6caae29a530f95703858
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-07-25 23:31:37 +0200

    [framework] add IDs to domains

--- analyzer/src/framework/domain.ml
+++ analyzer/src/framework/domain.ml
@@ -28,8 +28,6 @@ sig
 
   include Lattice.LATTICE
 
-  val id : t dom
-
   val init : Ast.program -> ('a, t) man -> 'a flow -> 'a flow option
 
   val exec_interface : Zone.t interface
@@ -41,6 +39,34 @@ sig
   val ask: Zone.t -> 'r Query.query -> ('a, t) man -> 'a flow -> 'r option
 end
 
-let domains : (string * (module DOMAIN)) list ref = ref []
-let register_domain name modl = domains := (name, modl) :: !domains
-let find_domain name = List.assoc name !domains
+type _ id = ..
+type (_, _) eq = Eq : ('a, 'a) eq
+
+type domain = Domain : {
+    name : string;
+    id : 'a id;
+    domain : (module DOMAIN with type t = 'a);
+    eq : 'b. 'b id -> ('a, 'b) eq option;
+  } -> domain
+
+let domains : domain list ref = ref []
+
+let register dom () =
+  domains := dom :: !domains
+
+let find_by_name d =
+  let Domain {domain} = List.find (function Domain {name} -> name = d) !domains in
+  let module D = (val domain : DOMAIN with type t = 'a) in
+  (module D : DOMAIN)
+
+let find_by_id : type a. a id -> (module DOMAIN with type t = a) = fun id ->
+  let rec aux : type a. a id -> domain list -> (module DOMAIN with type t = a) = fun id l ->
+    match l with
+    | [] -> raise Not_found
+    | hd :: tl ->
+      let Domain {eq; domain} = hd in
+      match eq id with
+      | Some Eq -> domain
+      | None -> aux id tl
+  in
+  aux id !domains
