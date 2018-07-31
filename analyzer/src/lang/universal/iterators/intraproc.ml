9a1af6ad69e845acaa788b03ea7c1345f65fd535
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-07-31 12:44:40 +0200

    [universal] add intraproc iterator

---
+++ analyzer/src/lang/universal/iterators/intraproc.ml
@@ -0,0 +1,60 @@
+(****************************************************************************)
+(*                   Copyright (C) 2017 The MOPSA Project                   *)
+(*                                                                          *)
+(*   This program is free software: you can redistribute it and/or modify   *)
+(*   it under the terms of the CeCILL license V2.1.                         *)
+(*                                                                          *)
+(****************************************************************************)
+
+(** Intra-procedural iterator handles blocks, assignments and tests *)
+
+open Framework.Essentials
+open Ast
+
+let name = "universal.iterators.intraproc"
+let debug fmt = Debug.debug ~channel:name fmt
+
+module Domain : Framework.Domains.Stateless.S =
+struct
+
+  let zone = Zone.Z_universal
+
+  let init prog man flow = None
+
+  let exec stmt man flow =
+    match skind stmt with
+    | S_expression(e) ->
+      Some (
+        man.eval e flow |>
+        Post.bind man @@ fun e flow ->
+        Post.singleton flow
+      )
+
+    | S_block(block) ->
+      Some (
+        List.fold_left (fun acc stmt -> man.exec stmt acc) flow block |>
+        Post.singleton
+      )
+
+    | S_if(cond, s1, s2) ->
+      let range = srange stmt in
+      let flow1 = man.exec (mk_assume cond range) flow |>
+                  man.exec s1
+      in
+      let flow2 = man.exec (mk_assume (mk_not cond range) range) flow |>
+                  man.exec s2
+      in
+      (* FIXME: propagate annotations in flow insensitive way *)
+      let flow' = Flow.join man flow1 flow2 in
+      Some (Post.singleton flow')
+
+    | _ -> None
+      
+  let eval exp man flow = None
+
+  let ask query man flow = None
+
+end
+
+let setup () =
+  Framework.Domains.Stateless.register_domain name (module Domain)
