c3730ecd3d36eb912ab591bad71330aa138a5ac5
Journault <matthieu.journault@lip6.fr>
2018-09-13 10:31:01 +0200

    aggregate added

---
+++ analyzer/src/lang/c/desugar/aggregate.ml
@@ -0,0 +1,100 @@
+(****************************************************************************)
+(*                   Copyright (C) 2017 The MOPSA Project                   *)
+(*                                                                          *)
+(*   This program is free software: you can redistribute it and/or modify   *)
+(*   it under the terms of the CeCILL license V2.1.                         *)
+(*                                                                          *)
+(****************************************************************************)
+
+(** array access and structure member access transformation to pointer
+   arithmetic*)
+
+open Framework.Essentials
+open Ast
+
+(** {2 Domain definition} *)
+(** ===================== *)
+
+module Domain : Framework.Domains.Stateless.S =
+struct
+
+  (** Domain identification *)
+  (** ===================== *)
+
+  type _ domain += D_c_desugar_array_struct : unit domain
+  let id = D_c_desugar_array_struct
+  let name = "c.desugar_array_struct"
+  let identify : type a. a domain -> (unit, a) eq option =
+    function
+    | D_c_desugar_array_struct -> Some Eq
+    | _ -> None
+
+  let debug fmt = Debug.debug ~channel:name fmt
+
+  (** Zoning definition *)
+  (** ================= *)
+
+  let zone = Zone.Z_c
+  let import_exec = [Universal.Zone.Z_universal]
+  let import_eval = []
+
+  (** Initialization *)
+  (** ============== *)
+
+  let init _ _ _ =
+    None
+
+  let exec _ _ _ = None
+
+  let eval exp man flow =
+    let range = erange exp in
+    match ekind exp with
+    | E_c_array_subscript(e1, e2) ->
+      let t = exp |> etyp |> Ast.pointer_type in
+      Eval.singleton (
+        {exp with
+         ekind = E_c_deref (
+             mk_expr
+               ~etyp:t
+               (E_binop(Universal.Ast.O_plus , e1, e2))
+               (tag_range range "pointer_arithmetic")
+           )
+        }) flow
+      |> Option.return
+    | E_c_member_access (e, i, f) ->
+      let t = etyp e in
+      if is_c_record_type t then
+        let x = align_byte t i in
+        let exp =
+          {exp with ekind =
+                      E_c_deref (
+                        mk_expr
+                          ~etyp:(t |> Ast.pointer_type)
+                          (E_binop(
+                              Universal.Ast.O_plus,
+                              (
+                                (mk_c_cast
+                                   (mk_c_address_of e (tag_range range "pointer_arithmetic &"))
+                                   (T_c_integer C_signed_char |> Ast.pointer_type)
+                                   (tag_range range "pointer_arithmetic cast")
+                                )
+                              ),
+                              (Universal.Ast.mk_int x (tag_range range "pointer_arithmetic int"))
+                            )
+                          )
+                          (tag_range range "pointer_arithmetic+")
+                      )
+          }
+        in
+        Eval.singleton exp flow
+        |> Option.return
+      else
+        Debug.fail "[c.desugar.aggregate] member_access on non record type"
+    | _ -> None
+
+  let ask _ _ _  = None
+
+end
+
+let () =
+  Framework.Domains.Stateless.register_domain (module Domain)
