21e6033d6b7122b76f7d1c44b70a9c68cbb05abc
Journault <matthieu.journault@lip6.fr>
2018-09-14 11:31:38 +0200

    machine integers v1

--- analyzer/src/lang/c/alarms.ml
+++ analyzer/src/lang/c/alarms.ml
@@ -5,11 +5,15 @@ type token +=
   | TOutOfBound of Framework.Location.range
   | TNullDeref of Framework.Location.range
   | TInvalidDeref of Framework.Location.range
+  | TDivideByZero of Framework.Location.range
+  | TIntegerOverflow of Framework.Location.range
 
 type alarm_kind +=
   | AOutOfBound
   | ANullDeref
   | AInvalidDeref
+  | ADivideByZero
+  | ATIntegerOverflow
 
 let () =
   register_token
@@ -17,6 +21,8 @@ let () =
           | TOutOfBound r -> Format.fprintf fmt "TOutOfBound %a" Framework.Location.pp_range r
           | TNullDeref r -> Format.fprintf fmt "TNullDeref %a" Framework.Location.pp_range r
           | TInvalidDeref r -> Format.fprintf fmt "TInvalidDeref %a" Framework.Location.pp_range r
+          | TDivideByZero r -> Format.fprintf fmt "TDivideByZero %a" Framework.Location.pp_range r
+          | TIntegerOverflow r -> Format.fprintf fmt "TIntegerOverflow %a" Framework.Location.pp_range r
           | tk -> next fmt tk
         );
       compare = (fun next a b ->
@@ -24,6 +30,8 @@ let () =
           | TOutOfBound x, TOutOfBound y -> Framework.Location.compare_range x y
           | TNullDeref x, TNullDeref y -> Framework.Location.compare_range x y
           | TInvalidDeref x, TInvalidDeref y -> Framework.Location.compare_range x y
+          | TDivideByZero x, TDivideByZero y -> Framework.Location.compare_range x y
+          | TIntegerOverflow x, TIntegerOverflow y -> Framework.Location.compare_range x y
           | _ -> next a b
         )
     };
@@ -34,12 +42,16 @@ let () =
           | AOutOfBound, AOutOfBound -> 0
           | ANullDeref, ANullDeref -> 0
           | AInvalidDeref, AInvalidDeref -> 0
+          | ADivideByZero, ADivideByZero -> 0
+          | ATIntegerOverflow, ATIntegerOverflow -> 0
           | _ -> default a b
         );
       print = (fun default fmt a -> match a.alarm_kind with
           | AOutOfBound -> Format.fprintf fmt "Out of bound access"
           | ANullDeref -> Format.fprintf fmt "Null dereference"
           | AInvalidDeref -> Format.fprintf fmt "Invalid dereference"
+          | ADivideByZero -> Format.fprintf fmt "Division by zero"
+          | ATIntegerOverflow -> Format.fprintf fmt "Integer overflow"
           | _ -> default fmt a
         )
     };
