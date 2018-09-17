129dd7f029754d2394a88452f634c8f183b0a531
Journault <matthieu.journault@lip6.fr>
2018-09-17 10:12:17 +0200

    [Universal.Heap] pool to stacked interface

--- analyzer/src/lang/universal/heap/pool.ml
+++ analyzer/src/lang/universal/heap/pool.ml
@@ -26,9 +26,9 @@ type t = {
   old : AddrSet.t;
 }
 
-(** For a given range and a given address kind, we distinguish the old and recent
-      addresses with a fixed encoding.
-*)
+(** For a given range and a given address kind, we distinguish the old
+    and recent addresses with a fixed encoding.  *)
+
 let old_uid = 0
 let recent_uid = 1
 
@@ -51,9 +51,10 @@ let top = {
 
 let is_top abs = AddrSet.is_top abs.recent && AddrSet.is_top abs.old
 
-let leq abs1 abs2 =
+let subset_intern abs1 abs2 =
   AddrSet.subset abs1.recent abs2.recent &&
-  AddrSet.subset abs1.old abs2.old
+   AddrSet.subset abs1.old abs2.old
+
 
 let mem_old addr abs = AddrSet.mem addr abs.old
 let mem_recent addr abs = AddrSet.mem addr abs.recent
@@ -62,19 +63,29 @@ let add_old (addr: addr) (abs: t) : t = {abs with old = AddrSet.add addr abs.old
 let add_recent (addr: addr) (abs: t) : t = {abs with recent =AddrSet.add addr abs.recent}
 
 
-let join annot abs1 abs2 = {
+let join_intern annot abs1 abs2 = {
   recent = AddrSet.join annot abs1.recent abs2.recent;
   old = AddrSet.join annot abs1.old abs2.old;
 }
 
-let meet annot abs1 abs2 = {
+let meet_intern annot abs1 abs2 = {
   recent = AddrSet.meet annot abs1.recent abs2.recent;
   old = AddrSet.meet annot abs1.old abs2.old;
 }
 
-let widening ctx = join
+let widening_intern ctx = join_intern
 
 let print fmt abs =
   Format.fprintf fmt "recent: @[%a@]@\nold: @[%a@]@\n"
     AddrSet.print abs.recent
     AddrSet.print abs.old
+
+let lift f =
+  fun annot subman (abs1, sub1) (abs2, sub2) ->
+    (f annot abs1 abs2, sub1, sub2)
+
+let join = lift join_intern
+let meet = lift meet_intern
+let widening = lift widening_intern
+let subset subman (abs1, sub1) (abs2, sub2) =
+  subset_intern abs1 abs2, sub1, sub2
