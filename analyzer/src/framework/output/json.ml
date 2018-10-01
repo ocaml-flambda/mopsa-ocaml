4635f80026310048e4f07f6a29457f8022e76c68
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-10-01 17:24:18 +0200

    [framework] refactor output rendering

---
+++ analyzer/src/framework/output/json.ml
@@ -0,0 +1,14 @@
+(****************************************************************************)
+(*                   Copyright (C) 2017 The MOPSA Project                   *)
+(*                                                                          *)
+(*   This program is free software: you can redistribute it and/or modify   *)
+(*   it under the terms of the CeCILL license V2.1.                         *)
+(*                                                                          *)
+(****************************************************************************)
+
+
+(** Format the results of the analysis in JSON. *)
+
+let render man alarms time files out = ()
+
+let panic exn files out = ()
---
+++ analyzer/src/framework/output/text.ml
@@ -0,0 +1,14 @@
+(****************************************************************************)
+(*                   Copyright (C) 2017 The MOPSA Project                   *)
+(*                                                                          *)
+(*   This program is free software: you can redistribute it and/or modify   *)
+(*   it under the terms of the CeCILL license V2.1.                         *)
+(*                                                                          *)
+(****************************************************************************)
+
+
+(** Display the results of the analysis in a textual form. *)
+
+let render man alarms time files out = ()
+
+let panic exn files out = ()
