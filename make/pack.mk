7aeca19ea126bad4cebcfa0f56c11d55aad8adc5
Antoine Miné <antoine.mine@lip6.fr>
2018-10-08 00:13:08 +0200

    [make] build bytecode version currently, both native and bytecode version are compiled TODO: allow native or bytecode version to be compiled without the other TODO: fix DLLPATHS to automatically include all dll paths

--- make/pack.mk
+++ make/pack.mk
@@ -5,7 +5,9 @@
 define generate_pack_flags =
  PACK_NAME_$(1) = $$(call pack_name,$(1))
  PACK_CMX_$(1) = $$(BUILD)/$(1).cmx
+ PACK_CMO_$(1) = $$(BUILD)/$(1).cmo
  INCLUDE_FLAG_$$(PACK_CMX_$(1)) = $$(patsubst $$(SRC)%,$$(BUILD)%, $$(call include_lineage,$$(SRC)/$(1)))
+ INCLUDE_FLAG_$$(PACK_CMO_$(1)) = $$(patsubst $$(SRC)%,$$(BUILD)%, $$(call include_lineage,$$(SRC)/$(1)))
  PACK_VAR_$(1) = $$(subst /,.,$(1))
  PACK_CONTENT_$(1) = \
 	$$(if $$($$(PACK_VAR_$(1))),\
@@ -13,8 +15,14 @@ define generate_pack_flags =
 		$$(foreach f,$$(wildcard $$(SRC)/$(1)/*.ml) $$(shell find $$(SRC)/$(1)/* -maxdepth 0 -type d),$$(shell basename $$(f) .ml))\
 	)
  PACK_DEP_$$(PACK_CMX_$(1)) = $$(patsubst %,$$(BUILD)/$(1)/%.cmx,$$(PACK_CONTENT_$(1)))
+ PACK_DEP_$$(PACK_CMO_$(1)) = $$(patsubst %,$$(BUILD)/$(1)/%.cmo,$$(PACK_CONTENT_$(1)))
  PARENT_PACK_$(1) = $$(patsubst /%,%,$$(patsubst $$(SRC)%,%,$$(shell realpath --relative-to=. $$(SRC)/$(1)/..)))
  PACK_FLAG_$$(PACK_CMX_$(1)) = \
+	$$(if $$(PARENT_PACK_$(1)),\
+		-for-pack $$(call pack_name,$$(PARENT_PACK_$(1))),\
+		\
+	)
+ PACK_FLAG_$$(PACK_CMO_$(1)) = \
 	$$(if $$(PARENT_PACK_$(1)),\
 		-for-pack $$(call pack_name,$$(PARENT_PACK_$(1))),\
 		\
