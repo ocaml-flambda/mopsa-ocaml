d7c6c43410d979be87c0c828810ba17d32b37f3f
Raphaël Monat <raphael.monat@lip6.fr>
2018-10-08 11:18:08 +0200

    [python] Port of desugar.bool finished, closes #45

--- analyzer/src/lang/python/desugar/bool.ml
+++ analyzer/src/lang/python/desugar/bool.ml
@@ -75,92 +75,65 @@ module Domain =
           *       | true, true, false -> Eval.singleton (mk_py_false range) flow
           *       | _ -> Eval.singleton (mk_py_top T_bool range) flow
           *       end *)
+
       (* E⟦ e1 is not e2 ⟧ *)
       | E_binop(O_py_is_not, e1, e2) ->
          man.eval (mk_not (mk_binop e1 O_py_is e2 range) range) flow |> Option.return
-      (* re_eval_singleton man.eval (Some (mk_not (mk_binop e1 O_py_is e2 range) range), flow, [])*)
 
       (* E⟦ e1 in e2 ⟧ *)
       | E_binop(O_py_in, e1, e2) ->
-         Debug.fail "FIXME: To implement"
-         (* Eval.eval_list [e1; e2] man.eval flow |>
-          *   Eval.bind (fun el flow ->
-          *       let e1, e2 = match el with [e1; e2] -> e1, e2 | _ -> assert false in
-          *       let o2 = object_of_expr e2 in
-          *       let cls2 = Addr.class_of_object o2 in
-          *       Eval.assume
-          *         (Utils.mk_object_hasattr cls2 "__contains__" range)
-          *         ~fthen:(fun true_flow ->
-          *           let exp' = mk_py_call (mk_py_object_attr cls2 "__contains__" range) [e2; e1] range in
-          *           man.eval exp' true_flow
-          *         )
-          *         ~felse:(fun false_flow ->
-          *           Eval.assume
-          *             (Utils.mk_object_hasattr cls2 "__iter__" range)
-          *             ~fthen:(fun true_flow ->
-          *               let v = mk_tmp () in
-          *               let stmt = mk_stmt (S_py_for (
-          *                                       mk_var v range,
-          *                                       e2,
-          *                                       mk_if
-          *                                         (mk_binop (mk_var v range) O_eq e1 range)
-          *                                         (mk_stmt S_break range)
-          *                                         (mk_block [] range)
-          *                                         range,
-          *                                       (mk_block [] range)
-          *                            )) range
-          *               in
-          *               let flow = man.exec stmt true_flow in
-          *               man.eval (mk_var v range) flow |> Eval.add_cleaners [mk_remove_var v range]
-          *             )
-          *             ~felse:(fun false_flow ->
-          *               Eval.assume
-          *                 (Utils.mk_object_hasattr cls2 "__getitem__" range)
-          *                 ~fthen:(fun true_flow ->
-          *                   Framework.Exceptions.panic_at range "evaluating 'in' operator using __getitem__ not supported"
-          *                 )
-          *                 ~felse:(fun false_flow ->
-          *                   let flow = man.exec (Utils.mk_builtin_raise "TypeError" range) false_flow in
-          *                   Eval.empty_singleton flow
-          *                 )
-          *                 man false_flow
-          *             )
-          *             man false_flow
-          *         ) man flow
-          *     ) |> Option.return *)
+         Eval.eval_list [e1; e2] man.eval flow |>
+           Eval.bind (fun el flow ->
+               let e1, e2 = match el with [e1; e2] -> e1, e2 | _ -> assert false in
+
+               man.eval (mk_py_call (mk_py_object (Addr.find_builtin "type") range) [e2] range) flow |>
+                 Eval.bind (fun cls2 flow ->
+                     Eval.assume
+                       (Utils.mk_hasattr cls2 "__contains__" range)
+                       ~fthen:(fun true_flow ->
+                         let exp' = mk_py_call (mk_py_attr cls2 "__contains__" range) [e2; e1] range in
+                         man.eval exp' true_flow
+                       )
+                       ~felse:(fun false_flow ->
+                         Eval.assume
+                           (Utils.mk_hasattr cls2 "__iter__" range)
+                           ~fthen:(fun true_flow ->
+                             let v = mk_tmp () in
+                             let stmt = mk_stmt (S_py_for (
+                                                     mk_var v range,
+                                                     e2,
+                                                     mk_if
+                                                       (mk_binop (mk_var v range) O_eq e1 range)
+                                                       (mk_stmt S_break range)
+                                                       (mk_block [] range)
+                                                       range,
+                                                     (mk_block [] range)
+                                          )) range
+                             in
+                             let flow = man.exec stmt true_flow in
+                             man.eval (mk_var v range) flow |> Eval.add_cleaners [mk_remove_var v range]
+                           )
+                           ~felse:(fun false_flow ->
+                             Eval.assume
+                               (Utils.mk_hasattr cls2 "__getitem__" range)
+                               ~fthen:(fun true_flow ->
+                                 Framework.Exceptions.panic_at range "evaluating 'in' operator using __getitem__ not supported"
+                               )
+                               ~felse:(fun false_flow ->
+                                 let flow = man.exec (Utils.mk_builtin_raise "TypeError" range) false_flow in
+                                 Eval.empty_singleton flow
+                               )
+                               man false_flow
+                           )
+                           man false_flow
+                       ) man flow
+                   )
+             )
+         |> Option.return
 
       (* E⟦ e1 in e2 ⟧ *)
       | E_binop(O_py_not_in, e1, e2) ->
          man.eval (mk_not (mk_binop e1 O_py_in e2 range) range) flow |> Option.return
-
-      (* E⟦ not e ⟧ *)
-      (* as S_assume, this is moved to a lower domain *)
-      (* | E_unop(Framework.Ast.O_log_not, e') (\*when is_py_expr e'*\) ->
-       *    Debug.fail "FIXME: To implement" *)
-         (* man.eval e' flow |>
-          *   Eval.bind (fun e' flow ->
-          *       let o = object_of_expr e' in
-          *       if Addr.isinstance o (Addr.find_builtin "bool") then
-          *         let e = value_of_object o in
-          *         let a = man.ask (Memory.Query.QInt e) flow |> Option.none_to_exn in
-          *         match Memory.Value.I.can_be_true a, Memory.Value.I.can_be_false a with
-          *         | true, false -> oeval_singleton (Some (mk_py_false range), flow, [])
-          *         | false, true -> oeval_singleton (Some (mk_py_true range), flow, [])
-          *         | true, true -> oeval_singleton (Some (mk_py_top T_bool range), flow, [])
-          *         | false, false -> oeval_singleton (None, flow, [])
-          *       else
-          *         man.eval (Utils.mk_builtin_call "bool" [e'] range) flow |>
-          *           eval_compose (fun ret flow ->
-          *               let o = object_of_expr ret in
-          *               let e = value_of_object o in
-          *               let a = man.ask (Memory.Query.QInt e) flow |> Option.none_to_exn in
-          *               match Memory.Value.I.can_be_true a, Memory.Value.I.can_be_false a with
-          *               | true, false -> oeval_singleton (Some (mk_py_false range), flow, [])
-          *               | false, true -> oeval_singleton (Some (mk_py_true range), flow, [])
-          *               | true, true -> oeval_singleton (Some (mk_py_top T_bool range), flow, [])
-          *               | false, false -> oeval_singleton (None, flow, [])
-          *             )
-          *     ) |> Option.return *)
 
       (* E⟦ e1 op e2 op e3 ... ⟧ *)
       | E_py_multi_compare(left, ops, rights) ->
@@ -190,51 +163,6 @@ module Domain =
       | _ -> None
 
 
-    (* let rec exec zone (stmt:Framework.Ast.stmt) (man:('a, unit) man) (flow:'a flow) : 'a post option =
-     *   (\* let range = srange stmt in *\)
-     *   match skind stmt with
-     *   (\* S⟦ ?e ⟧ *\)
-     (*   moved to a lower domain *)
-     *   | S_assume(e) when is_py_expr e ->
-     *      Debug.fail "FIXME: assume: To implement"
-     *
-     *      (\* let check_bool e ~otherwise flow =
-     *       *   let o = object_of_expr e in
-     *       *   (\\* Universal.Utils.assume_to_exec
-     *       *    *   (mk_py_call (mk_py_object (Addr.find_builtin "isinstance") range) [e; mk_py_object (Addr.find_builtin "bool") range] range)
-     *       *    *   (fun true_flow -> *\\)
-     *       *   if Addr.isinstance o (Addr.find_builtin "bool") then
-     *       *     let true_flow = flow in
-     *       *     let e = value_of_object o in
-     *       *     let a = man.ask (Memory.Query.QInt e) true_flow |> Option.none_to_exn in
-     *       *     begin match Memory.Value.I.can_be_true a, Memory.Value.I.can_be_false a with
-     *       *     | true, false -> true_flow
-     *       *     | false, true -> man.flow.set TCur man.env.bottom true_flow
-     *       *     | true, true -> true_flow
-     *       *     | false, false -> man.flow.set TCur man.env.bottom true_flow
-     *       *     end
-     *       *   else
-     *       *     otherwise flow
-     *       * in
-     *       *
-     *       * Some (man.eval e flow |>
-     *       *   Post.bind man (fun e flow ->
-     *       *       Post.of_flow @@
-     *       *         check_bool e
-     *       *           ~otherwise:(fun flow ->
-     *       *             let post =
-     *       *               Post.bind man (fun b flow ->
-     *       *                   Post.of_flow @@ check_bool b
-     *       *                                     ~otherwise:(fun flow ->
-     *       *                                       Framework.Exceptions.fail "call to bool returned a non boolean expression %a" Framework.Ast.pp_expr b
-     *       *                                     ) flow
-     *       *                 )
-     *       *                 (man.eval (Utils.mk_builtin_call "bool" [e] range) flow) in
-     *       *             post.Post.flow
-     *       *           ) flow
-     *       *   )) *\)
-     *
-     *   | _ -> None *)
     let exec _ _ _ _ = None
 
     let ask _ _ _ = None
