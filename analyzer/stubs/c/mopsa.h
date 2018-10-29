1b95058c88fc3d48da4a4e27748e050c503041f9
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2018-10-29 23:41:48 +0100

    [c] add some mopsa built-in stubs

---
+++ analyzer/stubs/c/mopsa.h
@@ -0,0 +1,46 @@
+#ifndef _MOPSA_H
+#define _MOPSA_H
+
+// Abstract values
+extern long int _mopsa_rand_int(long int, long int);
+extern unsigned long int _mopsa_rand_unsigned_long(unsigned long int, unsigned long int);
+
+extern char _mopsa_range_char();
+extern unsigned char _mopsa_range_unsigned_char();
+extern int _mopsa_range_int();
+extern unsigned int _mopsa_range_unsigned_int();
+extern short _mopsa_range_short();
+extern unsigned short _mopsa_range_unsigned_short();
+extern long _mopsa_range_long();
+extern unsigned long _mopsa_range_unsigned_long();
+extern long _mopsa_range_long_long();
+extern unsigned long _mopsa_range_unsigned_long_long();
+
+
+// Raise Framework.Manager.Panic exception with a given message
+extern void _mopsa_panic(const char*);
+
+// Printing
+extern void _mopsa_print();
+
+// Errors
+#define OUT_OF_BOUND 1
+#define NULL_DEREF 2
+#define INVALID_DEREF 3
+#define INTEGER_OVERFLOW 4
+#define DIVISION_BY_ZERO 5
+
+// Assertions
+extern void _mopsa_assert_true(int cond);
+extern void _mopsa_assert_exists(int cond);
+extern void _mopsa_assert_false(int cond);
+extern void _mopsa_assert_unreachable();
+extern void _mopsa_assert_safe();
+extern void _mopsa_assert_unsafe();
+extern void _mopsa_assert_error(int error);
+extern void _mopsa_assert_error_exists(int error);
+extern void _mopsa_assert_error_at_line(int error, int line);
+extern void _mopsa_set_debug_channels(char* channels);
+
+
+#endif //_MOPSA_H
