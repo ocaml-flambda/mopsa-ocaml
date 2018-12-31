4d866ea4411f85132b174e2ffeb09439b4eb28ee
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-01-01 00:35:36 +0100

    [stubs] fix macro expansion

--- parsers/c_stubs/src/c_stubs_parser/passes/macro_expansion.ml
+++ parsers/c_stubs/src/c_stubs_parser/passes/macro_expansion.ml
@@ -10,23 +10,118 @@
 
 let debug fmt = Debug.debug ~channel:"c_stubs.passes.macro_expansion" fmt
 
-let rec visit lexbuf out macros =
+
+let token_to_string = function
+  | Parser.INT_CONST(n) -> Z.to_string n
+  | FLOAT_CONST f -> string_of_float f
+  | CHAR_CONST n ->
+    let s =
+      let ss = string_of_int n in
+      if n < 10 then "00" ^ ss else
+      if n < 100 then "0" ^ ss
+      else ss
+    in
+    "'\\" ^ s ^ "'"
+  | STRING_CONST s -> "\"" ^ s ^ "\""
+  | INVALID -> "INVALID"
+  | IDENT x -> x
+  | AND -> "and"
+  | OR -> "or"
+  | IMPLIES -> "implies"
+  | NOT -> "not"
+  | ASSIGN -> "="
+  | PLUS -> "+"
+  | MINUS -> "-"
+  | DIV -> "/"
+  | MOD -> "%"
+  | LAND -> "&&"
+  | LOR -> "||"
+  | BAND -> "&"
+  | BOR -> "|"
+  | LNOT -> "!"
+  | BNOT -> "~"
+  | ARROW -> "->"
+  | STAR -> "*"
+  | GT -> ">"
+  | GE -> ">="
+  | LT -> "<"
+  | LE -> "<="
+  | EQ -> "=="
+  | NEQ -> "!="
+  | RSHIFT -> ">>"
+  | LSHIFT -> "<<"
+  | BXOR -> "^"
+  | LPAR -> "("
+  | RPAR -> ")"
+  | LBRACK -> "["
+  | RBRACK -> "]"
+  | COLON -> ":"
+  | SEMICOL -> ";"
+  | DOT -> "."
+  | COMMA -> ","
+  | PRIME -> "'"
+  | BEGIN -> "/*$"
+  | END -> "*/"
+  | EOF -> ""
+  | REQUIRES -> "requires"
+  | LOCAL -> "local"
+  | ASSIGNS -> "assigns"
+  | CASE -> "case"
+  | ASSUMES -> "assumes"
+  | ENSURES -> "ensures"
+  | PREDICATE -> "predicate"
+  | TRUE -> "true"
+  | FALSE -> "false"
+  | FORALL -> "forall"
+  | EXISTS -> "exists"
+  | IN -> "in"
+  | NEW -> "new"
+  | FREE -> "free"
+  | PRIMED -> "primed"
+  | RETURN  -> "return"
+  | SIZE  -> "size"
+  | SIZEOF  -> "sizeof"
+  | OFFSET  -> "offset"
+  | BASE  -> "base"
+  | PTR_VALID  -> "valid"
+  | FLOAT_VALID  -> "float_valid"
+  | FLOAT_INF -> "float_inf"
+  | FLOAT_NAN -> "float_nan"
+  | OLD -> "old"
+  | VOID -> "void"
+  | CHAR  -> "char"
+  | INT  -> "int"
+  | LONG  -> "long"
+  | FLOAT  -> "float"
+  | DOUBLE  -> "double"
+  | SHORT  -> "short"
+  | SIGNED -> "signed"
+  | UNSIGNED  -> "unsigned"
+  | CONST  -> "const"
+  | STRUCT -> "struct"
+  | UNION  -> "union"
+  | ENUM  -> "enum"
+
+
+let rec visit_macros lexbuf found out macros =
   let token = Lexer.read lexbuf in
-  let lexeme = Lexing.lexeme lexbuf in
+  let lexeme = token_to_string token in
+  debug "lexeme %s" lexeme;
   match token with
   | Parser.EOF ->
-    ()
+    found
 
   | Parser.IDENT(id)
     when MapExt.StringMap.mem id macros ->
     let content = MapExt.StringMap.find id macros in
     let lexbuf' = Lexing.from_string content in
-    visit lexbuf' out macros;
-    visit lexbuf out macros
+    let _ = visit_macros lexbuf' true out macros in
+    let _ = visit_macros lexbuf true out macros in
+    true
 
   | _ ->
     Buffer.add_string out (lexeme ^ " ");
-    visit lexbuf out macros
+    visit_macros lexbuf found out macros
 
 
 let doit stub prj =
@@ -41,5 +136,6 @@ let doit stub prj =
   in
   let lexbuf = Lexing.from_string stub in
   let out = Buffer.create (String.length stub) in
-  visit lexbuf out macros;
-  Buffer.contents out
+  let macros_found = visit_macros lexbuf false out macros in
+  let stub' = if macros_found then Buffer.contents out else stub in
+  stub'
