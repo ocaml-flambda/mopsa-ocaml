5145c0c8802cfec8d22f66f2c62d22fe522f3802
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-01-04 10:13:15 +0100

    [c] add unit tests for variable initialization with stubs

--- analyzer/tests/c/stubs/var_init_tests.c
+++ analyzer/tests/c/stubs/var_init_tests.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+
 /*$
  * ensures: x == 1;
  */
@@ -6,3 +8,31 @@ int x;
 void test_init_global_scalar() {
   _mopsa_assert(x == 1);
 }
+
+
+
+/*$
+ * ensures: a[0] == 0 and 
+ *          a[1] == 1 and
+ *          a[2] == 10;
+ */
+int a[10];
+
+void test_init_global_array() {
+  _mopsa_assert(a[0] == 0);
+  _mopsa_assert(a[1] == 1);
+  _mopsa_assert(a[2] == 10);
+}
+
+
+/*$
+ * local: void *addr = new MyResource;
+ * ensures: s == addr;
+ */
+void *s;
+
+void test_init_resource() {
+  _mopsa_assert(s != NULL);
+}
+
+
