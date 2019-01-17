d0e7fb0a01f664c04e27e2f8922f4399fd1930dd
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-01-17 17:54:16 +0100

    [c] put a random null-terminating string in argv[0]

--- analyzer/stubs/c/mopsa/mopsa.c
+++ analyzer/stubs/c/mopsa/mopsa.c
@@ -10,7 +10,7 @@ static int _argc;
 /*$
  * local:   char ** buf = new Memory;
  * ensures: size(buf) == _argc + 1; // need to allocate an additional
- *                                                    // pointer for the last NULL
+ *                                  // pointer for the last NULL
  * ensures: _argv == buf;
  */
 static char **_argv;
@@ -18,12 +18,21 @@ static char **_argv;
 /*$
  * local: char* str = new Memory;
  * ensures: size(str) in [1, INT_MAX]; // there is at least the NUL char at the end
- * // TODO ensures: str[size(str) - 1] == 0;  non-relational domains may fail here!
+ * // TODO ensures: str[size(str) - 1] == 0;
+ * ensures: return == str;
  */
 char* _mopsa_new_valid_string();
 
 void _mopsa_main() {
-  int i = 0;
+  // Add program name
+  _argv[0] = _mopsa_new_valid_string();
+
+  /*
+   * The following code produces false negative
+   * when analyzed with non-relational numeric domains
+   */
+  #if 0
+  int i = 1;
 
   // Initialize the array with valid strings
   while(i < _argc) {
@@ -33,4 +42,5 @@ void _mopsa_main() {
 
   // Add the last NULL pointer
   _argv[_argc] = NULL;
+  #endif
 }
