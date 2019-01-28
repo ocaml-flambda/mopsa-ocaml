aa19fa0c4a8286063270a72b669f09d54a011043
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-01-28 14:00:32 +0100

    [stubs] evaluate `implies` operator
    Fixes #3

--- analyzer/src/lang/stubs/iterator.ml
+++ analyzer/src/lang/stubs/iterator.ml
@@ -92,7 +92,16 @@ struct
       ftrue, ffalse
 
 
-    | F_binop (IMPLIES, f1, f2) -> panic_at f.range "IMPLIES not supported"
+    | F_binop (IMPLIES, f1, f2) ->
+      let ftrue1, ffalse1 = eval_formula f1 ~negate:true man flow in
+      let ffalse1 = OptionExt.none_to_exn ffalse1 in
+
+      let ftrue2, ffalse2 = eval_formula f2 ~negate man ftrue1 in
+
+      let ftrue = Flow.join man ffalse1 ftrue2 in
+
+      ftrue, ffalse2
+      
 
     | F_not ff ->
       let ftrue, ffalse = eval_formula ff ~negate:true man flow in
