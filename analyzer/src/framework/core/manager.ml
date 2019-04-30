dc0de216b30359ae952245f57af5169d0c3867ea
Raphaël Monat <raphael.monat@lip6.fr>
2019-04-30 18:06:20 +0200

    [framework] Manager: apply cleaners on all flows, even those with empty evals
    Typical example:
    `def to_pos(x):
        return x
    
    a = [1,2,3]
    a[to_pos(1)]`
    
    Before, the cleaners of the function call were not executed in the IndexError flow.

--- analyzer/src/framework/core/manager.ml
+++ analyzer/src/framework/core/manager.ml
@@ -215,7 +215,9 @@ let exec_eval
       (fun ctx e flow cleaners ->
          let flow = Flow.set_ctx ctx flow in
          match e with
-         | None -> ctx, flow
+         | None ->
+           let flow' = exec_block_on_all_flows cleaners man flow in
+           Flow.get_ctx flow', flow'
          | Some ee ->
            let flow' = f ee flow in
            let flow'' = exec_block_on_all_flows cleaners man flow' in
@@ -238,7 +240,9 @@ let post_eval
       (fun ctx e flow cleaners ->
          let flow = Flow.set_ctx ctx flow in
          match e with
-         | None -> ctx, Post.return flow
+         | None ->
+           let post' = exec_block_on_all_flows cleaners man flow |> Post.return in
+           Post.choose_ctx post', post'
          | Some ee ->
            let post = f ee flow in
            let post' = Post.bind (fun flow ->
@@ -260,14 +264,18 @@ let post_eval_with_cleaners
     (evl:('e, 'a) Eval.eval)
   : 'a post
   =
+  let f' e flow cleaners  =
+    match e with
+    | None ->
+      exec_block_on_all_flows cleaners man flow |> Post.return
+    | Some e ->
+      f e flow cleaners
+  in
   let ctx, ret = Eval.fold_apply
       (fun ctx e flow cleaners ->
          let flow = Flow.set_ctx ctx flow in
-         match e with
-         | None -> ctx, Post.return flow
-         | Some ee ->
-           let post = f ee flow cleaners in
-           Post.choose_ctx post, post
+         let post = f' e flow cleaners in
+         Post.choose_ctx post, post
       )
       Post.join
       (Post.meet man.lattice)
