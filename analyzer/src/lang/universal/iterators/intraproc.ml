43e6313d79e3417a1243305b6d01ce10689f4bc1
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-05-13 19:15:25 +0200

    [universal] fix match of S_expression

--- analyzer/src/lang/universal/iterators/intraproc.ml
+++ analyzer/src/lang/universal/iterators/intraproc.ml
@@ -41,7 +41,7 @@ struct
 
   let exec zone stmt man flow =
     match skind stmt with
-    | S_expression(e) when is_universal_type e.etyp ->
+    | S_expression(e) when is_universal_type e.etyp || e.etyp = T_any ->
       Some (
         man.eval e flow |>
         post_eval man @@ fun e flow ->
