3804ad38f50e12528249d708e3f472280c38cb3d
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-05-31 21:31:29 +0200

    [framework] cache: use arrays instead of list ref

--- analyzer/src/framework/core/cache.ml
+++ analyzer/src/framework/core/cache.ml
@@ -33,18 +33,84 @@ let debug fmt = Debug.debug ~channel:"framework.core.cache" fmt
 
 let opt_cache = ref 10
 
+
+(****************************************************************************)
+(**                             {2 Queue}                                   *)
+(****************************************************************************)
+
+module type KEY =
+sig
+  type t
+  val equal : t -> t -> bool
+end
+
+
+module Queue(Key:KEY) =
+struct
+
+  type 'a t = {
+    elements: (Key.t * 'a) option array;
+    size: int;
+    mutable next: int;
+  }
+
+
+  let create (size:int) : 'a t =
+    {
+      elements = Array.make size None;
+      size = size;
+      next = 0;
+    }
+
+
+  let add (k:Key.t) (v:'a) (q:'a t) : unit =
+    Array.set q.elements q.next (Some (k, v));
+    q.next <- (q.next + 1) mod q.size
+
+
+  let find (k:Key.t) (q:'a t) : 'a =
+    let prev i =
+      if i = 0 then q.size - 1 else i - 1
+    in
+
+    let rec aux i =
+      if i = q.next
+      then raise Not_found
+
+      else match Array.get q.elements i with
+        | None -> raise Not_found
+        | Some (k',v) when Key.equal k k' -> v
+        | _ -> aux (prev i)
+    in
+    aux (prev q.next)
+      
+
+end
+
+
+
+(****************************************************************************)
+(**                             {2 Cache}                                   *)
+(****************************************************************************)
+
 module Make(Domain: sig type t end) =
 struct
-  let exec_cache : ((zone * stmt * Domain.t Token.TokenMap.t) * Domain.t post) list ref = ref []
 
-  let eval_cache : (((zone * zone) * expr * Domain.t Token.TokenMap.t) * (expr, Domain.t) eval option) list ref = ref []
 
-  let add_to_cache : type a. a list ref -> a -> unit =
-    fun cache x ->
-      cache := x :: (
-          if List.length !cache < !opt_cache then !cache
-          else List.rev @@ List.tl @@ List.rev !cache
-        )
+  (** {2 Cache of post-conditions} *)
+  (** **************************** *)
+
+  module ExecCache = Queue(
+    struct
+      type t = zone * stmt * Domain.t Token.TokenMap.t
+      let equal (zone1,stmt1,tmap1) (zone2,stmt2,tmap2) =
+        zone1 = zone2 &&
+        stmt1 == stmt2 &&
+        tmap1 == tmap2
+    end
+    )
+
+  let exec_cache : Domain.t post ExecCache.t = ExecCache.create !opt_cache
 
   let exec f zone stmt man flow =
     let ff () =
@@ -65,34 +131,41 @@ struct
 
     else try
         let tmap = Flow.get_token_map flow in
-        let post =
-          List.find (fun ((zone',stmt',tmap'),_) ->
-              zone = zone' && stmt == stmt' && tmap == tmap'
-            ) !exec_cache |>
-          snd
-        in
+        let post = ExecCache.find (zone,stmt,tmap) exec_cache in
         Post.set_ctx (Flow.get_ctx flow) post
       with Not_found ->
         let post = ff () in
-        add_to_cache exec_cache ((zone, stmt, Flow.get_token_map flow), post);
+        ExecCache.add (zone, stmt, Flow.get_token_map flow) post exec_cache;
         post
 
+
+  (** {2 Cache of evaluations} *)
+  (** ************************ *)
+
+  module EvalCache = Queue(
+    struct
+      type t = (zone * zone) * expr * Domain.t Token.TokenMap.t
+      let equal (zone1,exp1,tmap1) (zone2,exp2,tmap2) =
+        zone1 = zone2 &&
+        exp1 == exp2 &&
+        tmap1 == tmap2
+    end
+    )
+
+  let eval_cache : (expr, Domain.t) eval option EvalCache.t = EvalCache.create !opt_cache
+
+ 
   let eval f zone exp man flow =
     if !opt_cache = 0
     then f exp man flow
 
     else try
         let tmap = Flow.get_token_map flow in
-        let evls =
-          List.find (fun ((zone',exp',tmap'),_) ->
-              zone = zone' && exp == exp' && tmap == tmap'
-            ) !eval_cache |>
-          snd
-        in
+        let evls = EvalCache.find (zone,exp,tmap) eval_cache in
         Option.lift (Eval.map_flow (Flow.copy_ctx flow)) evls
       with Not_found ->
         let evals = f exp man flow in
-        add_to_cache eval_cache ((zone, exp, Flow.get_token_map flow), evals);
+        EvalCache.add (zone, exp, Flow.get_token_map flow) evals eval_cache;
         evals
 
 end
