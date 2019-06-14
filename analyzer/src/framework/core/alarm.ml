0280030a1aafd839faf818f5d5db2d1ff13ae636
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-06-14 18:28:13 +0200

    [framework] minor changes

--- analyzer/src/framework/core/alarm.ml
+++ analyzer/src/framework/core/alarm.ml
@@ -114,15 +114,14 @@ let raise_alarm akind range ?(level = WARNING) ?(bottom=true) (lattice:'a lattic
   let cs = Callstack.get flow in
   let alarm = mk_alarm akind range ~cs in
   let tk = alarm_token alarm in
-  let a = Flow.get T_cur lattice flow in
-  let flow' =
-    if lattice.is_bottom a
-    then flow
-    else Flow.set tk lattice.top lattice flow
-  in
-  if bottom
-  then Flow.set T_cur lattice.bottom lattice flow'
-  else flow'
+  let a1 = Flow.get tk lattice flow in
+  let a2 = Flow.get T_cur lattice flow in
+  let aa = lattice.join (Flow.get_ctx flow |> Context.get_unit) a1 a2 in
+  let flow' = Flow.set tk aa lattice flow in
+  if bottom then
+    Flow.set T_cur lattice.bottom lattice flow'
+  else
+    flow'
 
 
 let () =
