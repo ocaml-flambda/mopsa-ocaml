210810324e297ca2a2fa08bc1ba53f22ce208722
Raphaël Monat <raphael.monat@lip6.fr>
2019-08-19 14:31:04 +0200

    [universal] Trying @a.ouadjaout solution for issue #14, v2

--- analyzer/src/lang/universal/iterators/interproc/inlining.ml
+++ analyzer/src/lang/universal/iterators/interproc/inlining.ml
@@ -32,26 +32,24 @@ open Callstack
 (** ===================== *)
 
 type token +=
-  | T_return of range * expr option
-  (** [T_return(l, ret)] represents flows reaching a return statement at
-      location [l]. The option expression [ret] keeps the returned expression
-      if present. *)
+  | T_return of range * bool
+  (** [T_return(l, b)] represents flows reaching a return statement at
+      location [l]. The boolean is true iff there a return expression is present *)
 
 let () =
   register_token {
     compare = (fun next tk1 tk2 ->
         match tk1, tk2 with
-        | T_return(r1, oe1), T_return(r2, oe2) ->
+        | T_return(r1, b1), T_return(r2, b2) ->
           (* we may return different things at one same location (for example due to disjunctions *)
           Compare.compose
             [ (fun () -> compare_range r1 r2);
-              (fun () -> Compare.option compare_expr oe1 oe2);
+              (fun () -> Pervasives.compare b1 b2);
             ]
         | _ -> next tk1 tk2
       );
     print = (fun next fmt -> function
-        | T_return(r, Some e) -> Format.fprintf fmt "return %a" pp_expr e
-        | T_return(r, None) -> Format.fprintf fmt "return"
+        | T_return(r, b) -> Format.fprintf fmt "return[r = %a, b = %b@" pp_range r b
         | tk -> next fmt tk
       );
   }
@@ -77,6 +75,18 @@ struct
     ieval = { provides = [Z_u, Z_any]; uses = [] };
   }
 
+  (* Context to keep return variable *)
+  let return_key =
+    let module K = Context.GenUnitKey(
+      struct
+        type t = (var * range) list
+        let print fmt vs =
+          Format.fprintf fmt "Return vars: %a" (Format.pp_print_list (fun fmt (v, r) -> Format.fprintf fmt "(%a at %a)" pp_var v pp_range r)) vs
+      end
+      )
+    in
+    K.key
+
   (** Initialization *)
   (** ============== *)
 
@@ -90,24 +100,24 @@ struct
   (** ============================== *)
 
   let exec zone stmt man flow =
+    let range = stmt.srange in
     match skind stmt with
     | S_return (Some e) ->
-      man.eval e flow |>
-      bind_some (fun ee flow ->
-          debug "on %a, flow = %a" pp_expr ee (Flow.print man.lattice.print) flow;
-          let cur = Flow.get T_cur man.lattice flow in
-          Flow.add (T_return (stmt.srange, Some ee)) cur man.lattice flow |>
-          Flow.remove T_cur |>
-          Post.return
-        )
-      |> Option.return
+      debug "Context = %a" Context.uprint (Context.get_unit @@ Flow.get_ctx flow);
+      let ret, rrange = List.hd @@ Context.find_unit return_key (Flow.get_ctx flow) in
+      let flow =
+        man.exec (mk_add_var ret rrange) flow |>
+        man.exec (mk_assign (mk_var ret rrange) e range) in
+      let cur = Flow.get T_cur man.lattice flow in
+      Flow.add (T_return (range, true)) cur man.lattice flow |>
+      Flow.remove T_cur |>
+      Post.return |> Option.return
 
     | S_return None ->
       let cur = Flow.get T_cur man.lattice flow in
-      Flow.add (T_return (stmt.srange, None)) cur man.lattice flow |>
+      Flow.add (T_return (range, false)) cur man.lattice flow |>
       Flow.remove T_cur |>
-          Post.return
-      |> Option.return
+      Post.return |> Option.return
 
     | _ -> None
 
@@ -149,18 +159,22 @@ struct
   let inline_function_exec_body man f args range new_vars flow ret =
     (* Check that no recursion is happening *)
 
+    let rets = try Context.find_unit return_key (Flow.get_ctx flow) with Not_found -> [] in
+    let flow = Flow.set_ctx
+        (Context.add_unit return_key ((ret, range)::rets) (Flow.get_ctx flow))
+        flow in
+
     let flow2 = man.exec f.fun_body flow in
 
     (* Iterate over return flows and assign the returned value to ret *)
     let flow3 =
       Flow.fold (fun acc tk env ->
           match tk with
-          | T_return(_, None) -> Flow.add T_cur env man.lattice acc
+          | T_return(_, false) ->
+            Flow.add T_cur env man.lattice acc
 
-          | T_return(_, Some e) ->
+          | T_return(_, true) ->
             Flow.set T_cur env man.lattice acc |>
-            man.exec (mk_add_var ret range) |>
-            man.exec (mk_assign (mk_var ret e.erange) e e.erange) |>
             Flow.join man.lattice acc
 
           | _ -> Flow.add tk env man.lattice acc
@@ -170,6 +184,7 @@ struct
     in
 
     (* Restore call stack *)
+    let flow3 = Flow.set_ctx (Context.add_unit return_key rets (Flow.get_ctx flow3)) flow3 in
     let _, flow3 = Flow.pop_callstack flow3 in
 
     (* Remove parameters and local variables from the environment *)
