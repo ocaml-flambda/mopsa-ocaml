8791a24ca9478f789717c8a8b0191e803db4e9fd
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-09-18 10:59:39 +0200

    [c] add a var_kind for numeric variables encoding C machine numbers

--- analyzer/src/lang/c/memory/packing/static_scope.ml
+++ analyzer/src/lang/c/memory/packing/static_scope.ml
@@ -132,7 +132,7 @@ struct
       compare_var ret v = 0
     with Not_found ->
       false
-  
+
   (** Packs of a base memory block *)
   let packs_of_base ctx b =
     match b with
@@ -146,7 +146,6 @@ struct
 
     | V { vkind = V_cvar {cvar_scope = Variable_parameter f} } ->
       (* Parameters are part of the caller and the callee packs *)
-      debug "parameter of %s" f.c_func_org_name; 
       let cs = Context.ufind Callstack.ctx_key ctx in
       if Callstack.is_empty cs then Set.empty
       else
@@ -160,9 +159,8 @@ struct
       (* Return variables are also part of the caller and the callee packs *)
       (* Note that the top of the callstack is not always the callee
          function, because the return variable is used after the function
-         returns 
+         returns
       *)
-      debug "return of %a" pp_expr call; 
       let cs = Context.ufind Callstack.ctx_key ctx in
       if Callstack.is_empty cs then Set.empty
       else
@@ -177,6 +175,7 @@ struct
         else
           let f2, _ = Callstack.pop cs' in
           Set.of_list [Locals f1.call_fun; Locals f2.call_fun]
+
     | V { vkind = V_tmp _ } ->
       (* Temporary variables are considered as locals *)
       let cs = Context.ufind Callstack.ctx_key ctx in
@@ -184,29 +183,25 @@ struct
       else
         let callee, _ = Callstack.pop cs in
         Set.singleton (Locals callee.call_fun)
+
     | _ ->
-      debug "empty base pack";
       Set.empty
 
-
-  let packs_of_base ctx b =
-    let packs = packs_of_base ctx b in
-    debug "packs(%a) = %a" pp_base b (Set.fprint SetExt.printer_default print) packs;
-    packs
 
 
   (** Packing function returning packs of a variable *)
   let rec packs_of_var ctx prev v =
-    debug "packs_of_var(%a)" pp_var v;
     match v.vkind with
     | V_cvar _ -> packs_of_base ctx (V v)
     | Lowlevel.Cells.Domain.V_c_cell {base} -> packs_of_base ctx base
     | Lowlevel.String_length.Domain.V_c_string_length (base,_) -> packs_of_base ctx base
     | Scalars.Pointers.Domain.Domain.V_c_ptr_offset vv -> packs_of_var ctx prev vv
+    | Scalars.Machine_numbers.Domain.V_c_num vv -> packs_of_var ctx prev vv
     | _ ->
       match prev with
-      | None -> debug "empty var pack"; Set.empty
+      | None -> Set.empty
       | Some e -> packs_of_expr ctx e
+
 
   and packs_of_expr ctx e =
     match e.ekind with
@@ -249,16 +244,14 @@ struct
         if Map.mem pack acc then acc else Map.add pack Domain.top acc
       ) packs m
     in
-    debug "%a = %a;" pp_expr v pp_expr e;
+
     (* Execute pointwise *)
     Map.mapi (fun pack a ->
         if Set.mem pack packs then
           let _,e' = resolve_missing_variables ctx pack e in
-          debug "   assigning %a in pack %a" pp_expr e' print pack;
           let stmt' = mk_assign v e' range in
           Domain.exec ctx stmt' a |> Option.none_to_exn
         else
-          let () = debug "   skipping pack %a" print pack in
           a
       ) m
 
