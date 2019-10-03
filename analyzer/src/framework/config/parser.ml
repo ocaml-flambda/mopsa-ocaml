c2f7e3b16e3f98c8c05d4609da1562dd1f3444f9
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-10-03 15:20:09 +0200

    [framework] begin the reduced product at lowlevel signatures

--- analyzer/src/framework/config/parser.ml
+++ analyzer/src/framework/config/parser.ml
@@ -76,12 +76,9 @@ let spec = {
 
   product = (fun abstraction signature ->
       match abstraction, signature with
-      | _, S_lowlevel -> true
-
       | A_domain, S_simplified -> true
-
+      | A_stack, S_lowlevel -> true
       | A_stack, S_intermediate -> true
-
       | _ -> false
     );
 
@@ -255,6 +252,7 @@ and stack_lowlevel config : (module Sig.Stacked.Lowlevel.STACK) =
   | S_leaf name -> Sig.Stacked.Lowlevel.find_stack name
   | S_chain(op, l) -> stack_chain_lowlevel op l
   | S_cast c -> stack_cast_lowlevel c
+  | S_product (l,r) -> stack_product_lowlevel l r
   | _ -> assert false
 
 
@@ -279,6 +277,16 @@ and stack_chain_lowlevel (op:operator) (l:config list) : (module Sig.Stacked.Low
     | _ -> assert false
 
 
+and stack_product_lowlevel (l:config list) (rules:string list) : (module Sig.Stacked.Lowlevel.STACK) =
+  let ll = List.map stack_lowlevel l in
+  let rules = List.map (fun name ->
+      try Sig.Stacked.Reduction.find_eval_reduction name
+      with Not_found -> Exceptions.panic "reduction %s not found" name
+    ) rules
+  in
+  Transformers.Stacked.Lowlevel.Product.make ll rules
+
+
 and stack_cast_lowlevel (config:config) : (module Sig.Stacked.Lowlevel.STACK) =
   match config.signature with
   | S_intermediate ->
@@ -300,6 +308,7 @@ and stack_cast_lowlevel (config:config) : (module Sig.Stacked.Lowlevel.STACK) =
            "Stack signature %a can not be casted to signature %a"
            pp_signature s
            pp_signature S_lowlevel
+
 
 and stack_intermediate (config:config) : (module Sig.Stacked.Intermediate.STACK) =
   match config.structure with
