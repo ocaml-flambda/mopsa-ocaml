856efd8b3ba484240d55c5db06be430188618819
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-11-29 10:57:48 +0100

    [c] formatted io: warn when an argument type is incorrect

--- analyzer/src/lang/c/libs/clib/formatted_io/fscanf.ml
+++ analyzer/src/lang/c/libs/clib/formatted_io/fscanf.ml
@@ -59,7 +59,12 @@ struct
     }
   }
 
-  let alarms = [A_c_insufficient_format_args_cls; A_c_null_deref_cls; A_c_invalid_deref_cls; A_c_use_after_free_cls]
+  let alarms = [
+    A_c_insufficient_format_args_cls;
+    A_c_null_deref_cls;
+    A_c_invalid_deref_cls;
+    A_c_use_after_free_cls
+  ]
 
   (** {2 Transfer functions} *)
   (** ====================== *)
@@ -78,12 +83,24 @@ struct
     | Int t ->
       let typ = T_c_integer t in
       let ptr = T_c_pointer typ in
+      if not (is_c_pointer_type arg.etyp) || not (is_c_int_type @@ under_type arg.etyp) then
+        warn_at arg.erange "format expects argument of type '%a', but %a has type '%a'"
+          pp_typ ptr
+          pp_expr (get_orig_expr arg)
+          pp_typ arg.etyp
+      ;
       assert_valid_ptr arg range man flow >>$ fun () flow ->
       man.post (mk_assign (mk_c_deref (mk_c_cast arg ptr range) range) (mk_top typ range) range) flow
 
     | Float t ->
       let typ = T_c_float t in
       let ptr = T_c_pointer typ in
+      if not (is_c_pointer_type arg.etyp) || not (is_c_float_type @@ under_type arg.etyp) then
+        warn_at arg.erange "format expects argument of type '%a', but %a has type '%a'"
+          pp_typ ptr
+          pp_expr (get_orig_expr arg)
+          pp_typ arg.etyp
+      ;
       assert_valid_ptr arg range man flow >>$ fun () flow ->
       man.post (mk_assign (mk_c_deref (mk_c_cast arg ptr range) range) (mk_top typ range) range) flow
 
@@ -91,6 +108,11 @@ struct
       assert false
 
     | String ->
+      if not (is_c_pointer_type arg.etyp) then
+        warn_at arg.erange "format expects a pointer argument, but %a has type '%a'"
+          pp_expr (get_orig_expr arg)
+          pp_typ arg.etyp
+      ;
       let w = match placeholder.ip_width with
         | None -> mk_top ul range
         | Some n -> mk_int (n-1) ~typ:ul range
