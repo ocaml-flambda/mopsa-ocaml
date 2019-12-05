b67080b474f4d795c586cc98ad54d46354e7d12b
Abdelraouf Ouadjaout <abdelraouf.ouadjaout@lip6.fr>
2019-12-05 03:11:16 +0100

    [universal] add a hook to show the progress of the analysis

---
+++ analyzer/src/lang/universal/hooks/progress.ml
@@ -0,0 +1,181 @@
+(****************************************************************************)
+(*                                                                          *)
+(* This file is part of MOPSA, a Modular Open Platform for Static Analysis. *)
+(*                                                                          *)
+(* Copyright (C) 2017-2019 The MOPSA Project.                               *)
+(*                                                                          *)
+(* This program is free software: you can redistribute it and/or modify     *)
+(* it under the terms of the GNU Lesser General Public License as published *)
+(* by the Free Software Foundation, either version 3 of the License, or     *)
+(* (at your option) any later version.                                      *)
+(*                                                                          *)
+(* This program is distributed in the hope that it will be useful,          *)
+(* but WITHOUT ANY WARRANTY; without even the implied warranty of           *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *)
+(* GNU Lesser General Public License for more details.                      *)
+(*                                                                          *)
+(* You should have received a copy of the GNU Lesser General Public License *)
+(* along with this program.  If not, see <http://www.gnu.org/licenses/>.    *)
+(*                                                                          *)
+(****************************************************************************)
+
+(** Hook for displaying progress of the analysis *)
+
+open Location
+open Mopsa
+open Format
+open Ast
+
+
+module Hook =
+struct
+
+  (** {2 Hook header} *)
+  (** *************** *)
+
+  let name = "progress"
+  let exec_zones = [Z_any]
+  let eval_zones = [Z_any,Z_any]
+
+
+  (** {2 Progression table entries} *)
+  (** ***************************** *)
+
+  (** Set of location ranges *)
+  module RangeSet = SetExt.Make(struct type t = range let compare = compare_range end)
+
+  (** Entry of the progression table *)
+  type entry = {
+    name: string; (** Name of the function *)
+    range: range; (** Location of the function *)
+    all_stmt_range_set: RangeSet.t; (** Location of all statements in the function body *)
+    mutable analyzed_stmt_range_set: RangeSet.t; (** Locations of analyzed statements *)
+    nb_all_stmt: int; (** Total number of statements *)
+    mutable nb_analyzed_stmt: int; (** Number of analyzed statements *)
+  }
+
+  (** Print a progression entry *)
+  let pp_entry fmt e =
+    fprintf fmt "%s: %d%%" e.name (100*e.nb_analyzed_stmt/e.nb_all_stmt)
+
+
+  (** {2 Progression table} *)
+  (** ********************* *)
+
+  (** Progression table *)
+  let table : entry Stack.t = Stack.create ()
+
+  let clear_line () =
+    printf "\027[2K\r@?"
+
+  let up_line () =
+    printf "\027[1A@?"
+
+  let print_head () =
+    printf "%a@?" pp_entry (Stack.top table)
+
+  let print_after_push () =
+    printf "@.%a@?" pp_entry (Stack.top table)
+
+  let print_after_pop () =
+    clear_line ();
+    if Stack.is_empty table then ()
+    else (
+      up_line ();
+      clear_line ();
+      print_head ()
+    )
+
+  let print_after_update () =
+    clear_line ();
+    print_head ()
+
+
+  (** Return the set of statements in the body of a function *)
+  let get_function_statements f =
+    Visitor.fold_stmt
+      (fun acc e -> VisitParts acc)
+      (fun acc s ->
+         match skind s with
+         | S_block _ -> VisitParts acc
+         | _ -> VisitParts (RangeSet.add s.srange acc)
+      ) RangeSet.empty f.fun_body
+
+
+  (** Insert a new entry in the progress table *)
+  let push f =
+    let all_stmt_range_set = get_function_statements f in
+    let entry = {
+      name = f.fun_name;
+      range = f.fun_range;
+      all_stmt_range_set;
+      analyzed_stmt_range_set = RangeSet.empty;
+      nb_all_stmt = RangeSet.cardinal all_stmt_range_set;
+      nb_analyzed_stmt = 0;
+    }
+    in
+    Stack.push entry table;
+    print_after_push ()
+
+
+  (** Update the progress table after a statement is analyzed *)
+  let update range =
+    if Stack.is_empty table
+    then ()
+    else (
+      let entry = Stack.top table in
+      if not @@ RangeSet.mem range entry.all_stmt_range_set then ()
+      else if RangeSet.mem range entry.analyzed_stmt_range_set then ()
+      else (
+        entry.analyzed_stmt_range_set <- RangeSet.add range entry.analyzed_stmt_range_set;
+        entry.nb_analyzed_stmt <- entry.nb_analyzed_stmt + 1;
+        print_after_update ()
+      )
+    )
+
+
+  (** Remove the head function from the progress table *)
+  let pop () =
+    let _ = Stack.pop table in
+    print_after_pop ();
+    ()
+  
+
+  
+
+  (** {2 Initialization} *)
+  (** ****************** *)
+
+
+  let init ctx = ()
+
+
+
+  (** {2 Events handlers} *)
+  (** ******************* *)
+
+  let on_before_exec zone stmt man flow =
+    ()
+
+
+  let on_after_exec zone stmt man post =
+    update stmt.srange
+
+
+  let on_before_eval zone exp man flow =
+    match ekind exp with
+    | E_call ({ ekind = E_function (User_defined f) }, args) -> push f
+    | _ -> ()
+
+
+  let on_after_eval zone exp man evl =
+    match ekind exp with
+    | E_call ({ ekind = E_function (User_defined f) }, args) -> pop ()
+    | _ -> ()
+
+  let on_finish man flow = ()
+
+end
+
+let () =
+  Core.Hook.register_stateless_hook (module Hook)
