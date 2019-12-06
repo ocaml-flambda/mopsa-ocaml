c8d456dfa40f6103d5390529641572790ca8fba4
Raphaël Monat <raphael.monat@lip6.fr>
2019-12-06 11:44:22 +0100

    [python] Add time.pyi stub

---
+++ share/mopsa/stubs/python/typeshed/time.pyi
@@ -0,0 +1,26 @@
+from typing import Any, Tuple, Union, Optional
+
+_TimeTuple = Tuple[int, int, int, int, int, int, int, int, int]
+
+altzone: int
+daylight: int
+timezone: int
+tzname: Tuple[str, str]
+
+
+class struct_time:
+    tm_year: int
+    tm_mon: int
+    tm_mday: int
+    tm_hour: int
+    tm_min: int
+    tm_sec: int
+    tm_wday: int
+    tm_yday: int
+    tm_isdst: int
+    tm_zone: str
+    tm_gmtoff: int
+
+
+def strftime(format: str, t: Union[_TimeTuple, struct_time] = ...) -> str: ...
+def strptime(string: str, format: str = ...) -> struct_time: ...
