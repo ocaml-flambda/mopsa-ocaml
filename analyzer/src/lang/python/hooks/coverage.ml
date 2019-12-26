467193f021a3d93db280036a470d4c366bd83c44
Raphaël Monat <raphael.monat@lip6.fr>
2019-12-26 11:58:06 +0100

    [python] Coverage hook supporting dead code detection

--- analyzer/src/lang/python/hooks/coverage.ml
+++ analyzer/src/lang/python/hooks/coverage.ml
@@ -57,8 +57,8 @@ struct
   type entry = {
     mutable never_analyzed_exprs : ExprSet.t;
     mutable never_analyzed_stmts : StmtSet.t;
-    (* mutable always_bottom_exprs : ExprSet.t;
-     * mutable always_bottom_stmts : StmtSet.t; *)
+    mutable always_bottom_exprs : ExprSet.t;
+    mutable always_bottom_stmts : StmtSet.t;
     mutable reachable_exprs : ExprSet.t;
     mutable reachable_stmts : StmtSet.t;
   }
@@ -98,12 +98,15 @@ struct
     Hashtbl.add table filename
       {never_analyzed_exprs = init_exprs;
        never_analyzed_stmts = init_stmts;
-       (* always_bottom_exprs = ExprSet.empty;
-        * always_bottom_stmts = StmtSet.empty; *)
+       always_bottom_exprs = ExprSet.empty;
+       always_bottom_stmts = StmtSet.empty;
        reachable_exprs = ExprSet.empty;
        reachable_stmts = StmtSet.empty}
 
   let init ctx = ()
+
+  let is_cur_bottom man flow =
+    man.lattice.is_bottom (Flow.get T_cur man.lattice flow)
 
   let on_before_exec zone stmt man flow =
     match skind stmt with
@@ -115,9 +118,14 @@ struct
       let file = get_range_file range in
       let entry = Hashtbl.find table file in
       if StmtSet.mem stmt entry.never_analyzed_stmts then
-        let () = entry.never_analyzed_stmts <- StmtSet.remove stmt  entry.never_analyzed_stmts in
-        entry.reachable_stmts <- StmtSet.add stmt entry.reachable_stmts
-
+        let () = entry.never_analyzed_stmts <- StmtSet.remove stmt entry.never_analyzed_stmts in
+        if is_cur_bottom man flow then
+          entry.always_bottom_stmts <- StmtSet.add stmt entry.always_bottom_stmts
+        else
+          entry.reachable_stmts <- StmtSet.add stmt entry.reachable_stmts
+      else if StmtSet.mem stmt entry.always_bottom_stmts && not @@ is_cur_bottom man flow then
+          let () = entry.always_bottom_stmts <- StmtSet.remove stmt entry.always_bottom_stmts in
+          entry.reachable_stmts <- StmtSet.add stmt entry.reachable_stmts
 
   let on_after_exec zone stmt man post = ()
   (*   match skind stmt with
@@ -140,13 +148,18 @@ struct
    * (\* si TOUT les post_exec sur une range sont à bottom on peut ajouter une info? Par contre il faut se méfier si seulement une seule chose est à bottom et pas les autres *\) *)
 
 
-
   let on_before_eval zone exp man flow =
     let range = erange exp in
     let file = get_range_file range in
     let entry = Hashtbl.find table file in
     if ExprSet.mem exp entry.never_analyzed_exprs then
       let () = entry.never_analyzed_exprs <- ExprSet.remove exp  entry.never_analyzed_exprs in
+      if is_cur_bottom man flow then
+        entry.always_bottom_exprs <- ExprSet.add exp entry.always_bottom_exprs
+      else
+        entry.reachable_exprs <- ExprSet.add exp entry.reachable_exprs
+    else if ExprSet.mem exp entry.always_bottom_exprs && not @@ is_cur_bottom man flow then
+      let () = entry.always_bottom_exprs <- ExprSet.remove exp entry.always_bottom_exprs in
       entry.reachable_exprs <- ExprSet.add exp entry.reachable_exprs
 
   let on_after_eval zone exp man evl =
@@ -160,7 +173,7 @@ struct
     let open Unix in
     let open Filename in
     let time = Unix.localtime (Unix.gettimeofday ()) in
-    let dirname = Format.asprintf "/tmp/coverage_%d_%d_%d_%d%d%d"
+    let dirname = Format.asprintf "/tmp/coverage_%2d_%2d_%4d_%2d%2d%2d"
         time.tm_mday
         (time.tm_mon+1)
         (time.tm_year+1900)
@@ -168,31 +181,41 @@ struct
         time.tm_min
         time.tm_sec in
     let () = Unix.mkdir dirname 0o755 in
+    Format.printf "Coverage:@.";
+    Hashtbl.iter (fun filename entry ->
+        let whole_size = ListExt.fold_left (+) 0 (List.map StmtSet.cardinal [entry.never_analyzed_stmts; entry.always_bottom_stmts; entry.reachable_stmts]) in
+        let size = ListExt.fold_left (+) 0 (List.map StmtSet.cardinal [entry.always_bottom_stmts; entry.reachable_stmts]) in
+        Format.printf "\t%s: %.2f%@." filename (100. *. (float_of_int size) /. (float_of_int whole_size))
+      ) table;
     Hashtbl.iter (fun filename entry ->
         let fname = remove_extension @@ basename filename in
         let oc = open_out (dirname ^ "/" ^ fname ^ ".cov") in
         let ocf = Format.formatter_of_out_channel oc in
         let file = open_in filename in
         let rec process_file lineno =
+          let search_stmt = (fun s ->
+              let r = s.srange in
+              let start = get_range_start r in
+              let stop = get_range_end r in
+              (* if *) get_pos_line start <= lineno && lineno <= get_pos_line stop (* then *)
+              (*   let () = Debug.debug ~channel:"coverage" "%d: %a" lineno pp_stmt s in true
+                 * else false *)
+            ) in
+          let search_expr = (fun e ->
+                let r = e.erange in
+                let start = get_range_start r in
+                let stop = get_range_end r in
+                get_pos_line start = lineno && lineno = get_pos_line stop) in
           try
             let l = input_line file in
             if is_comment l then
               Format.fprintf ocf "%s@." l
-            else if StmtSet.exists (fun s ->
-                let r = s.srange in
-                let start = get_range_start r in
-                let stop = get_range_end r in
-                if get_pos_line start <= lineno && lineno <= get_pos_line stop then
-                  let () = Debug.debug ~channel:"coverage" "%d: %a" lineno pp_stmt s in true
-                else false
-              ) entry.reachable_stmts then (* okay, statement reached *)
+            else if StmtSet.exists search_stmt entry.reachable_stmts then (* okay, statement reached *)
               Format.fprintf ocf "\027[38;5;%dm%s\027[0m@." (List.assoc "green" Debug.colors) l
+            else if StmtSet.exists search_stmt entry.always_bottom_stmts then
+              Format.fprintf ocf "\027[38;5;%dm%s\027[0m@." (List.assoc "yellow" Debug.colors) l
             else
-              begin match ExprSet.find_first_opt (fun e ->
-                let r = e.erange in
-                let start = get_range_start r in
-                let stop = get_range_end r in
-                get_pos_line start = lineno && lineno = get_pos_line stop) entry.reachable_exprs with
+              begin match ExprSet.find_first_opt search_expr entry.reachable_exprs with
               | Some e ->
                 let range = e.erange in
                 let n = String.length l in
@@ -204,8 +227,21 @@ struct
                   (String.sub l cols (cole-cols))
                   (String.sub l cole (n-cole))
               | None ->
+                begin match ExprSet.find_first_opt search_expr entry.always_bottom_exprs with
+                  | Some e ->
+                    let range = e.erange in
+                    let n = String.length l in
+                    let cols, cole = get_pos_column @@ get_range_start range,
+                                     get_pos_column @@ get_range_end range in
+                    Format.fprintf ocf "%s\027[38;5;%dm%s\027[0m%s@."
+                      (String.sub l 0 cols)
+                      (List.assoc "yellow" Debug.colors)
+                      (String.sub l cols (cole-cols))
+                      (String.sub l cole (n-cole))
+                  | None ->
                 (* statement not analyzed *)
-                Format.fprintf ocf "\027[38;5;%dm%s\027[0m@." (List.assoc "red" Debug.colors) l
+                    Format.fprintf ocf "\027[38;5;%dm%s\027[0m@." (List.assoc "red" Debug.colors) l
+                end
               end;
             process_file (lineno+1)
           with End_of_file -> close_in file in
