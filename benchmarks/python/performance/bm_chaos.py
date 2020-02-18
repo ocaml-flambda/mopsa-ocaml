a9bfb3be0e254fa18086254968130fbdf9a4908c
Raphaël Monat <raphael.monat@lip6.fr>
2020-02-18 11:08:26 +0100

    [ci] Fix python ci

--- benchmarks/python/performance/bm_chaos.py
+++ benchmarks/python/performance/bm_chaos.py
@@ -353,6 +353,7 @@ def test_values():
     main()
     mopsa.ignore_exception(ValueError)
     mopsa.ignore_exception(OverflowError)
+    mopsa.ignore_exception(IndexError)
     # mopsa.assert_exception_exists(TypeError) the typeerror is no longer raised due to a better precision
     # mopsa.ignore_exception(TypeError)
     mopsa.assert_safe()
