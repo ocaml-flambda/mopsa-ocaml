8655133200af096f4803bd3da00b93c82ab8ec49
Raphaël Monat <raphael.monat@lip6.fr>
2020-02-29 15:12:09 +0100

    [python] Fix py_ellipsis in import

--- analyzer/src/lang/python/desugar/import.ml
+++ analyzer/src/lang/python/desugar/import.ml
@@ -399,9 +399,9 @@ module Domain =
           {stmt with skind = S_block (newblock, [])}, newglobals, newflow
 
         | S_expression {ekind = (E_constant C_py_ellipsis)}
-        | S_expression {ekind = (E_constant (C_string _))}
-        (* | Universal.Heap.Recency.S_perform_gc ->
-         *   {stmt with skind = S_block ([], [])}, globals, flow *)
+        | S_expression {ekind = (E_constant (C_string _))} ->
+          (* | Universal.Heap.Recency.S_perform_gc -> *)
+            {stmt with skind = S_block ([], [])}, globals, flow
 
         | _ -> panic_at range "stmt %a not supported in stubs file %s" pp_stmt stmt name
       in
