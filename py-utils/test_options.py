71c46e88bc91c25267b569b1b845e71744dec52b
Raphaël Monat <raphael.monat@lip6.fr>
2020-03-04 17:20:49 +0100

    [python] GC test script: keep GC time

--- py-utils/test_options.py
+++ py-utils/test_options.py
@@ -4,6 +4,7 @@ import subprocess
 import collections
 
 file_dir = "/home/raphael/these/pybenchs/"
+gctime_str = "Total gc time : "
 
 def run_mopsa(mopsa_name, mopsa_options, file_name, runs=1):
     rt = 0
@@ -13,14 +14,19 @@ def run_mopsa(mopsa_name, mopsa_options, file_name, runs=1):
         stdout, stderr = proc.communicate()
         stop = time.time()
         rt += stop - start
-    return rt / runs
+    return rt / runs, stdout
 
 def f(percent=100):
-    options = { "GC": ["-gc", f"-gc-percent={percent}"] }
+    options = { "GC": ["-gc", f"-gc-percent={percent}", "-hook gctest"] }
     print(f"GC percent: {percent}")
     base_config = { "Type Analysis": "../bin/mopsa-python-types",
                 "Value Analysis": "../bin/mopsa-python" }
-    files = ["bm_chaos.py", "bm_deltablue.py", "bm_fannkuch.py", "bm_float.py", "bm_go.py", "bm_hexiom.py", "bm_nbody.py", "bm_nqueens.py", "bm_pidigits.py", "bm_raytrace.py", "bm_regex_v8.py", "bm_richards.py", "bm_scimark.py", "bm_spectral_norm.py", "bm_unpack_sequence.py"]
+    files = ["bm_chaos.py", "bm_deltablue.py",
+             "bm_fannkuch.py", "bm_float.py", "bm_go.py"
+             "bm_hexiom.py", "bm_nbody.py", "bm_nqueens.py",
+             "bm_pidigits.py", "bm_raytrace.py", "bm_regex_v8.py",
+             "bm_richards.py", "bm_scimark.py",
+             "bm_spectral_norm.py", "bm_unpack_sequence.py"]
     s = "| Filename              "
     for config_name, config_str in base_config.items():
         for option_name, option_str in options.items():
@@ -37,19 +43,23 @@ def f(percent=100):
         try:
             for config_name, config_str in base_config.items():
                 for option_name, option_str in options.items():
-                    t_noopt = run_mopsa(config_str, [], f)
-                    t_opt = run_mopsa(config_str, option_str, f)
+                    t_noopt, _ = run_mopsa(config_str, [], f)
+                    t_opt, stdout = run_mopsa(config_str, option_str, f)
+
+                    stdout = stdout.decode()
+                    gc_time = float(stdout[stdout.find(gctime_str)+len(gctime_str):].split()[0])
+
                     total_noopt[config_name, option_name] += t_noopt
                     total_opt[config_name, option_name] += t_opt
                     err = 100 * (t_noopt - t_opt) / t_noopt
-                    print(f"| {t_noopt:19.3f}s | {t_opt:19.3f}s | {err:6.2f}% ", end="", flush=True)
+                    print(f"| {t_noopt:19.3f}s | {t_opt:10.3f}s ({gc_time:5.3f}s) | {err:6.2f}% ", end="", flush=True)
             print("|")
         except KeyboardInterrupt:
             print()
             continue
 
     total = "Total"
-    print(f"| {total:20} ", end="", flush=True)
+    print(f"| {total:21} ", end="", flush=True)
     for config_name, config_str in base_config.items():
         for option_name, option_str in options.items():
             err = 100 * (total_noopt[config_name, option_name] - total_opt[config_name, option_name]) / total_noopt[config_name, option_name]
