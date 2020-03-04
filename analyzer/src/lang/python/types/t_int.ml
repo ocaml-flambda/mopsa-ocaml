f307df0bd6a13bf24e497a59104599459073b818
Raphaël Monat <raphael.monat@lip6.fr>
2020-03-04 17:33:19 +0100

    [python] Merge true+false only when the flow is the same

--- analyzer/src/lang/python/types/t_int.ml
+++ analyzer/src/lang/python/types/t_int.ml
@@ -54,12 +54,15 @@ struct
     if Cases.cardinal c = 2 then
       let () = debug "2 cases, we're good!" in
       let t, f, oflow = Cases.fold (fun oe flow (ftrue, ffalse, oflow) ->
-                            match oe with
-                            | Some e when compare_expr e (mk_py_object (Addr_env.addr_true (), Some (mk_int 1 ~typ:T_int e.erange)) e.erange) = 0 ->
-                               (true, ffalse, OptionExt.apply (fun flow' -> Some (Flow.join man.lattice flow flow')) (Some flow) oflow)
-                            | Some e when compare_expr e (mk_py_object (Addr_env.addr_false (), Some (mk_int 0 ~typ:T_int e.erange)) e.erange) = 0 ->
-                               (ftrue, true, OptionExt.apply (fun flow' -> Some (Flow.join man.lattice flow flow')) (Some flow) oflow)
-                            | _ -> (ftrue, ffalse, oflow)
+                            if oflow <> None && Flow.subset man.lattice flow (OptionExt.none_to_exn oflow) then
+                              (ftrue, ffalse, oflow)
+                            else
+                              match oe with
+                              | Some e when compare_expr e (mk_py_object (Addr_env.addr_true (), Some (mk_int 1 ~typ:T_int e.erange)) e.erange) = 0 ->
+                                 (true, ffalse, OptionExt.apply (fun flow' -> Some (Flow.join man.lattice flow flow')) (Some flow) oflow)
+                              | Some e when compare_expr e (mk_py_object (Addr_env.addr_false (), Some (mk_int 0 ~typ:T_int e.erange)) e.erange) = 0 ->
+                                 (ftrue, true, OptionExt.apply (fun flow' -> Some (Flow.join man.lattice flow flow')) (Some flow) oflow)
+                              | _ -> (ftrue, ffalse, oflow)
                           ) c (false, false, None) in
       let () = debug "t = %b, f = %b" t f in
       if t && f then
@@ -161,7 +164,7 @@ struct
                           ~zone:Universal.Zone.Z_u_int
                           ~fthen:(fun flow -> man.eval (mk_py_true range) flow)
                           ~felse:(fun flow -> man.eval (mk_py_false range) flow)
-                      |> merge_tf_top man range
+                      (* |> merge_tf_top man range *)
                           (* man.eval ~zone:(Zone.Z_py, Zone.Z_py_obj) (mk_py_top T_bool range) true_flow *)
                       )
                     ~felse:(fun false_flow ->
