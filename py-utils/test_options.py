3ab3d862ed12b5762a20527c79120a6f192c5f0b
Raphaël Monat <raphael.monat@lip6.fr>
2020-03-06 20:52:39 +0100

    [python] GC option script: remove old benchs

--- py-utils/test_options.py
+++ py-utils/test_options.py
@@ -3,7 +3,7 @@ import time
 import subprocess
 import collections
 
-file_dir = "/home/raphael/these/pybenchs/"
+file_dir = "../../pybenchs/"
 gctime_str = "Total gc time : "
 
 def run_mopsa(mopsa_name, mopsa_options, file_name, runs=1):
@@ -28,10 +28,11 @@ def f(percent=100, global_options=None):
     print(f"GC percent: {percent}, Global options: {global_options}")
     base_config = { "Type Analysis": "../bin/mopsa-python-types",
                 "Value Analysis": "../bin/mopsa-python" }
-    files = ["bm_chaos.py", "bm_deltablue.py",
+    files = ["bm_chaos.py", #"bm_deltablue.py",
              "bm_fannkuch.py", "bm_float.py", "bm_go.py",
-             "bm_hexiom.py", "bm_nbody.py", "bm_nqueens.py",
-             "bm_pidigits.py", "bm_raytrace.py", "bm_regex_v8.py",
+             "bm_hexiom.py", "bm_nbody.py", 
+             # "bm_pidigits.py", 
+             "bm_raytrace.py", "bm_regex_v8.py",
              "bm_richards.py", "bm_scimark.py",
              "bm_spectral_norm.py", "bm_unpack_sequence.py"]
     s = "| Filename              "
@@ -39,7 +40,7 @@ def f(percent=100, global_options=None):
         for option_name, option_str in options.items():
             name1 = f"{config_name}"
             name2 = f"{name1} + {option_name}"
-            s = f"{s}| {name1:20} | {name2:23} | speedup "
+            s = f"{s}| {name1:19} | {name2:23} | speedup "
 
     print(s + "|")
 
@@ -58,7 +59,7 @@ def f(percent=100, global_options=None):
                     total_noopt[config_name, option_name] += t_noopt
                     total_opt[config_name, option_name] += t_opt
                     err = 100 * (t_noopt - t_opt) / t_noopt
-                    print(f"| {t_noopt:13.3f}s ({errs_noopt:>3}) | {t_opt:8.3f}s ({gc_time:5.3f}s, {errs_opt:>3}) | {err:6.2f}% ", end="", flush=True)
+                    print(f"| {t_noopt:13.3f}s ({errs_noopt:>2}) | {t_opt:9.3f}s ({gc_time:5.3f}s, {errs_opt:>2}) | {err:6.2f}% ", end="", flush=True)
             print("|")
         except KeyboardInterrupt:
             print()
@@ -69,7 +70,7 @@ def f(percent=100, global_options=None):
     for config_name, config_str in base_config.items():
         for option_name, option_str in options.items():
             err = 100 * (total_noopt[config_name, option_name] - total_opt[config_name, option_name]) / total_noopt[config_name, option_name]
-            print(f"| {total_noopt[config_name, option_name]:19.3f}s | {total_opt[config_name, option_name]:22.3f}s | {err:6.2f}% ", end="", flush=True)
+            print(f"| {total_noopt[config_name, option_name]:18.3f}s | {total_opt[config_name, option_name]:24.3f}s | {err:6.2f}% ", end="", flush=True)
     print("|\n")
 
 
