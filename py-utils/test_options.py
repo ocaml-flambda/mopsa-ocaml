4a73f19be70c24b6447f1cc91e63aa5990f0673b
Raphaël Monat <raphael.monat@lip6.fr>
2020-03-06 15:44:22 +0100

    [python] GC test script: different global allocation policies

--- py-utils/test_options.py
+++ py-utils/test_options.py
@@ -14,15 +14,22 @@ def run_mopsa(mopsa_name, mopsa_options, file_name, runs=1):
         stdout, stderr = proc.communicate()
         stop = time.time()
         rt += stop - start
-    return rt / runs, stdout
+    stdout = stdout.decode()
+    try:
+        nalarms = int(stdout.split()[-3])
+    except ValueError:
+        nalarms = 0
+    return rt / runs, nalarms, stdout
 
-def f(percent=100):
+def f(percent=100, global_options=None):
+    if global_options is None:
+        global_options = []
     options = { "GC": ["-gc", f"-gc-percent={percent}", "-hook gctest"] }
-    print(f"GC percent: {percent}")
+    print(f"GC percent: {percent}, Global options: {global_options}")
     base_config = { "Type Analysis": "../bin/mopsa-python-types",
                 "Value Analysis": "../bin/mopsa-python" }
     files = ["bm_chaos.py", "bm_deltablue.py",
-             "bm_fannkuch.py", "bm_float.py", "bm_go.py"
+             "bm_fannkuch.py", "bm_float.py", "bm_go.py",
              "bm_hexiom.py", "bm_nbody.py", "bm_nqueens.py",
              "bm_pidigits.py", "bm_raytrace.py", "bm_regex_v8.py",
              "bm_richards.py", "bm_scimark.py",
@@ -32,7 +39,7 @@ def f(percent=100):
         for option_name, option_str in options.items():
             name1 = f"{config_name}"
             name2 = f"{name1} + {option_name}"
-            s = f"{s}| {name1:20} | {name2:20} | speedup "
+            s = f"{s}| {name1:20} | {name2:23} | speedup "
 
     print(s + "|")
 
@@ -43,16 +50,15 @@ def f(percent=100):
         try:
             for config_name, config_str in base_config.items():
                 for option_name, option_str in options.items():
-                    t_noopt, _ = run_mopsa(config_str, [], f)
-                    t_opt, stdout = run_mopsa(config_str, option_str, f)
+                    t_noopt, errs_noopt, _ = run_mopsa(config_str, global_options, f)
+                    t_opt, errs_opt, stdout = run_mopsa(config_str, global_options + option_str, f)
 
-                    stdout = stdout.decode()
                     gc_time = float(stdout[stdout.find(gctime_str)+len(gctime_str):].split()[0])
 
                     total_noopt[config_name, option_name] += t_noopt
                     total_opt[config_name, option_name] += t_opt
                     err = 100 * (t_noopt - t_opt) / t_noopt
-                    print(f"| {t_noopt:19.3f}s | {t_opt:10.3f}s ({gc_time:5.3f}s) | {err:6.2f}% ", end="", flush=True)
+                    print(f"| {t_noopt:13.3f}s ({errs_noopt:>3}) | {t_opt:8.3f}s ({gc_time:5.3f}s, {errs_opt:>3}) | {err:6.2f}% ", end="", flush=True)
             print("|")
         except KeyboardInterrupt:
             print()
@@ -63,10 +69,13 @@ def f(percent=100):
     for config_name, config_str in base_config.items():
         for option_name, option_str in options.items():
             err = 100 * (total_noopt[config_name, option_name] - total_opt[config_name, option_name]) / total_noopt[config_name, option_name]
-            print(f"| {total_noopt[config_name, option_name]:19.3f}s | {total_opt[config_name, option_name]:19.3f}s | {err:6.2f}% ", end="", flush=True)
+            print(f"| {total_noopt[config_name, option_name]:19.3f}s | {total_opt[config_name, option_name]:22.3f}s | {err:6.2f}% ", end="", flush=True)
     print("|\n")
 
 
 if __name__ == "__main__":
-    for x in [1, 25, 50, 75, 100]:
-        f(x)
+#    for x in reversed([1, 12, 25, 37, 50, 62, 75, 87, 100]):
+    f(100, ["-default-alloc-pol=all"])
+    f(100, ["-default-alloc-pol=range"])
+    f(100, ["-default-alloc-pol=callstack"])
+    f(100, ["-default-alloc-pol=range_callstack"])
