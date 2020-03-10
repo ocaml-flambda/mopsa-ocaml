599b4a45ca4c4d93f3eade4c3e6e5bd79c1bc37e
Raphaël Monat <raphael.monat@lip6.fr>
2020-03-10 15:08:19 +0100

    [python] Gc test script: categorize alarms

--- py-utils/test_options.py
+++ py-utils/test_options.py
@@ -3,10 +3,14 @@ import time
 import subprocess
 import collections
 import os
+import re
 
 file_dir = "../../pybenchs/"
 gctime_str = "Total gc time : "
 ocaml_gc_stats = "v=0x400"
+
+
+results = {}
 
 def run_mopsa(mopsa_name, mopsa_options, file_name, runs=1, cwd=None, file_dir=""):
     rt = 0
@@ -18,12 +22,11 @@ def run_mopsa(mopsa_name, mopsa_options, file_name, runs=1, cwd=None, file_dir="
         rt += stop - start
     stdout = stdout.decode()
     stderr = stderr.decode()
-    try:
-        nalarms = int(stdout[stdout.find("Total: ") + len("Total: "):].split()[0])
-    except ValueError:
-        nalarms = 0
+    with open("logs/" + mopsa_name.replace("..", "_").replace("/", "_") + "_".join(mopsa_options) + "_" + file_name, 'w+') as f:
+        f.write(stdout)
+    alarms = re.findall("Uncaught Python exception: ([a-zA-Z-_][a-zA-Z0-9_-]*)", stdout)
     usedmem = int(int(stderr.split()[-3]) * 8 / 10**6) # this should be ocaml's top_heap_words, now in MB
-    return rt / runs, nalarms, stdout, usedmem
+    return rt / runs, alarms, stdout, usedmem
 
 def f(percent=100, global_options=None):
     if global_options is None:
@@ -43,7 +46,7 @@ def f(percent=100, global_options=None):
         for option_name, option_str in options.items():
             name1 = f"{config_name}"
             name2 = f"{name1} + {option_name}"
-            s = f"{s}| {name1:21} | {name2:29} | improvement (s, MB) "
+            s = f"{s}| {name1:25} | {name2:33} | improvement (s, MB) "
 
     print(s + "|")
 
@@ -56,14 +59,20 @@ def f(percent=100, global_options=None):
                 for option_name, option_str in options.items():
                     t_noopt, errs_noopt, _, usedmem_noopt = run_mopsa(config_str, global_options, f, file_dir=file_dir)
                     t_opt, errs_opt, stdout, usedmem_opt = run_mopsa(config_str, global_options + option_str, f, file_dir=file_dir)
-
+                    errs_opt_ty = errs_opt.count("TypeError") + errs_opt.count("AttributeError")
+                    errs_opt_val = len(errs_opt) - errs_opt_ty
+                    errs_noopt_ty = errs_noopt.count("TypeError") + errs_noopt.count("AttributeError")
+                    errs_noopt_val = len(errs_noopt) - errs_noopt_ty
+                    assert(errs_noopt == errs_opt)
                     gc_time = float(stdout[stdout.find(gctime_str)+len(gctime_str):].split()[0])
 
                     total_noopt[config_name, option_name] += t_noopt
                     total_opt[config_name, option_name] += t_opt
+                    results[f, config_name, option_name, True] = (t_opt, errs_opt, usedmem_opt)
+                    results[f, config_name, option_name, False] = (t_noopt, errs_noopt, usedmem_noopt)
                     err = (100 * (t_noopt - t_opt) / t_noopt).__round__()
                     mem = (100 * (usedmem_noopt - usedmem_opt) / usedmem_noopt).__round__()
-                    print(f"| {t_noopt:7.3f}s ({errs_noopt:>2}, {usedmem_noopt:>4}MB) | {t_opt:7.3f}s ({gc_time:5.3f}s, {errs_opt:>2}, {usedmem_opt:>4}MB) |    {err:3}%    {mem:3}%     ", end="", flush=True)
+                    print(f"| {t_noopt:7.3f}s ({errs_noopt_ty:>2}, {errs_noopt_val:>2}, {usedmem_noopt:>4}MB) | {t_opt:7.3f}s ({gc_time:5.3f}s, {errs_opt_ty:>2}, {errs_opt_val:>2}, {usedmem_opt:>4}MB) |    {err:3}%    {mem:3}%     ", end="", flush=True)
             print("|")
         except KeyboardInterrupt:
             print()
@@ -78,27 +87,35 @@ def f(percent=100, global_options=None):
                 for option_name, option_str in options.items():
                     t_noopt, errs_noopt, _, usedmem_noopt = run_mopsa(cwd + "/" + config_str, global_options, f, cwd=fpp_dir)
                     t_opt, errs_opt, stdout, usedmem_opt = run_mopsa(cwd + "/" + config_str, global_options + option_str, f, cwd=fpp_dir)
-
+                    errs_opt_ty = errs_opt.count("TypeError") + errs_opt.count("AttributeError")
+                    errs_opt_val = len(errs_opt) - errs_opt_ty
+                    errs_noopt_ty = errs_noopt.count("TypeError") + errs_noopt.count("AttributeError")
+                    errs_noopt_val = len(errs_noopt) - errs_noopt_ty
+                    assert(errs_noopt == errs_opt)
                     gc_time = float(stdout[stdout.find(gctime_str)+len(gctime_str):].split()[0])
 
                     total_noopt[config_name, option_name] += t_noopt
                     total_opt[config_name, option_name] += t_opt
+                    results[f, config_name, option_name, True] = (t_opt, errs_opt, usedmem_opt)
+                    results[f, config_name, option_name, False] = (t_noopt, errs_noopt, usedmem_noopt)
                     err = (100 * (t_noopt - t_opt) / t_noopt).__round__()
                     mem = (100 * (usedmem_noopt - usedmem_opt) / usedmem_noopt).__round__()
-                    print(f"| {t_noopt:7.3f}s ({errs_noopt:>2}, {usedmem_noopt:>4}MB) | {t_opt:7.3f}s ({gc_time:5.3f}s, {errs_opt:>2}, {usedmem_opt:>4}MB) |    {err:3}%    {mem:3}%     ", end="", flush=True)
+                    print(f"| {t_noopt:7.3f}s ({errs_noopt_ty:>2}, {errs_noopt_val:>2}, {usedmem_noopt:>4}MB) | {t_opt:7.3f}s ({gc_time:5.3f}s, {errs_opt_ty:>2}, {errs_opt_val:>2}, {usedmem_opt:>4}MB) |    {err:3}%    {mem:3}%     ", end="", flush=True)
             print("|")
         except KeyboardInterrupt:
             print()
             continue
         subprocess.Popen(["bash", "toggle_usage.sh"], cwd=fpp_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
 
+    with open(f"results_{percent}_{'_'.join(global_options)}", "w+") as f:
+        f.write(str(results))
 
     total = "Total"
     print(f"| {total:21} ", end="", flush=True)
     for config_name, config_str in base_config.items():
         for option_name, option_str in options.items():
             err = (100 * (total_noopt[config_name, option_name] - total_opt[config_name, option_name]) / total_noopt[config_name, option_name]).__round__()
-            print(f"| {total_noopt[config_name, option_name]:18.3f}s | {total_opt[config_name, option_name]:26.3f}s | {err:6.2f}% ", end="", flush=True)
+            print(f"| {total_noopt[config_name, option_name]:24.3f}s | {total_opt[config_name, option_name]:32.3f}s | {err:18.2f}% ", end="", flush=True)
     print("|\n")
 
 
