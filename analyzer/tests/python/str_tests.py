94f30aef7f4ea99e4b980b7b371cc63f6b644a6d
Raphaël Monat <raphael.monat@lip6.fr>
2020-10-06 10:59:55 +0200

    [python] Fix type annotations to work with nonrel

--- analyzer/tests/python/str_tests.py
+++ analyzer/tests/python/str_tests.py
@@ -4,3 +4,12 @@ def test_len():
     s = "hello"
     l = len(s)
     mopsa.assert_equal(l, 5)
+
+def test_add():
+    s1 = "hello"
+    s2 = " world"
+    mopsa.assert_equal(s1 + s2, "hello world")
+
+def test_mul():
+    s = "a" * 5
+    mopsa.assert_equal(s, "aaaaa")
