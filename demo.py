cf6096d17d655e568581b9a7813744db17c35065
Raphaël Monat <raphael.monat@lip6.fr>
2021-01-06 11:01:50 +0100

    [cpython] First demo: stubs are now in separate file

--- demo.py
+++ demo.py
@@ -10,17 +10,16 @@ a = A(1)
 
 c = Cbox(a)
 
-a.a = 42
+# a.a = 42
 
-ccontent = c.contents #getcontents().a
+# ccontent = c.contents #getcontents().a
 
-cz = c.counter
-cr = c.incr()
-# c.contents = 42
-c.counter = 4
-cf = c.counter
-c.counter = 10**1000 # should raise an OverflowError
-# Next: check READONLY flag, try with integers...
-
-
-b = broken()
+# cz = c.counter
+# cr = c.incr()
+# # c.contents = 42
+# c.counter = 4
+# cf = c.counter
+# c.counter = 10**10
+# # c.counter = 10**1000 # should raise an OverflowError
+# d = c.counter
+# b = broken()
