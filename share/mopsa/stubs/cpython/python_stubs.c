72426007bf874787a5bdfe9eb1f51cd347b6b379
Raphaël Monat <raphael.monat@lip6.fr>
2021-01-06 11:01:26 +0100

    [cpython] python stubs C code

---
+++ share/mopsa/stubs/cpython/python_stubs.c
@@ -0,0 +1,181 @@
+// stubs used by the analysis
+PyObject* exc_state = NULL;
+char* exc_msg = NULL;
+
+PyObject*
+PyErr_NoMemory()
+{
+    exc_state = PyExc_MemoryError;
+//    _mopsa_print();
+    return NULL;
+}
+
+PyObject*
+PyErr_Occurred()
+{
+    return exc_state;
+};
+
+void
+PyErr_Clear()
+{
+    exc_state = NULL;
+    exc_msg = NULL;
+}
+
+void PyErr_SetNone(PyObject* exc) {
+    exc_state = exc;
+}
+
+void PyErr_SetString(PyObject* exc, const char* msg){
+    exc_state = exc;
+    exc_msg = msg;
+//    _mopsa_print();
+}
+
+void PyLong_AsLong_Helper(){
+    PyErr_SetString(PyExc_OverflowError,
+                    "Python int too large to convert to C long");
+}
+
+
+int PyType_ReadyCheat(PyTypeObject *type)
+{
+    Py_TYPE(type) = &PyType_Type;
+    type->tp_alloc = PyType_GenericAlloc;
+    return 0;
+}
+
+PyObject*
+PyType_GenericAlloc(PyTypeObject *type, Py_ssize_t nitems)
+{
+    PyObject *obj;
+    const size_t size = _PyObject_VAR_SIZE(type, nitems+1);
+
+    obj = PyType_GenericAlloc_Helper(type, size);
+    memset(obj, '\0', size);
+
+    /* somehow PyObject_INIT? */
+    Py_TYPE(obj) = type;
+    obj->ob_refcnt = 1;
+
+    /* FIXME */
+    if (obj == NULL)
+        return PyErr_NoMemory();
+
+    return obj;
+}
+
+PyObject*
+_PyObject_New(PyTypeObject *type)
+{
+    PyObject *obj;
+    const size_t size = _PyObject_SIZE(type);
+    obj = PyType_GenericAlloc_Helper(type, size);
+    memset(obj, '\0', size);
+
+    if (obj == NULL)
+        return PyErr_NoMemory();
+
+    Py_TYPE(obj) = type;
+    obj->ob_refcnt = 1;
+
+    return obj;
+}
+
+static PyObject *
+// remove member_get and directly call PyMember_GetOne?
+// was PyMemberDescrObject, we're cheating again
+member_get(PyMemberDef *descr, PyObject *obj, PyObject *type)
+{
+    /* PyObject *res; */
+
+    /* if (descr_check((PyDescrObject *)descr, obj, &res)) */
+    /*     return res; */
+
+    /* if (descr->d_member->flags & READ_RESTRICTED) { */
+    /*     if (PySys_Audit("object.__getattr__", "Os", */
+    /*         obj ? obj : Py_None, descr->d_member->name) < 0) { */
+    /*         return NULL; */
+    /*     } */
+    /* } */
+    return PyMember_GetOne((char *)obj, descr); // was descr->d_member, we're cheating
+}
+
+// FIXME: we want to check the flag at creation rather than first call
+// FIXME: simplified version, check the real one in Python/structmember.c
+PyObject *
+PyMember_GetOne(const char *addr, PyMemberDef *l)
+{
+    PyObject *v;
+
+    addr += l->offset;
+    switch (l->type) {
+    case T_INT:
+        v = PyLong_FromLong(*(int*)addr);
+        break;
+    case T_OBJECT:
+        v = *(PyObject **)addr;
+        if (v == NULL)
+            v = Py_None;
+       /* Py_INCREF(v); */
+        break;
+    default:
+        PyErr_SetString(PyExc_SystemError, "bad memberdescr type");
+        v = NULL;
+    }
+//    _mopsa_print();
+    return v;
+}
+
+// FIXME: returns values with wrap_descr_set
+int
+PyMember_SetOne(char *addr, PyMemberDef *l, PyObject *v)
+{
+    PyObject *oldv;
+    addr += l->offset;
+
+    if ((l->flags & READONLY))
+    {
+        PyErr_SetString(PyExc_AttributeError, "readonly attribute");
+        return -1;
+    }
+    if (v == NULL) {
+        if (l->type == T_OBJECT_EX) {
+            /* Check if the attribute is set. */
+            if (*(PyObject **)addr == NULL) {
+                PyErr_SetString(PyExc_AttributeError, l->name);
+                return -1;
+            }
+        }
+        else if (l->type != T_OBJECT) {
+            PyErr_SetString(PyExc_TypeError,
+                            "can't delete numeric/char attribute");
+            return -1;
+        }
+    }
+    switch (l->type) {
+    case T_INT:{
+        long long_val = PyLong_AsLong(v);
+        if ((long_val == -1) && PyErr_Occurred())
+            return -1;
+        *(int *)addr = (int)long_val;
+        if ((long_val > INT_MAX) || (long_val < INT_MIN))
+            WARN("Truncation of value to int");
+        break;
+        }
+    case T_OBJECT:
+    case T_OBJECT_EX:
+        Py_XINCREF(v);
+        oldv = *(PyObject **)addr;
+        *(PyObject **)addr = v;
+        Py_XDECREF(oldv);
+        break;
+    default:
+        PyErr_Format(PyExc_SystemError,
+                     "bad memberdescr type for %s", l->name);
+        return -1;
+    }
+    return 0;
+}
+// end of stubs
