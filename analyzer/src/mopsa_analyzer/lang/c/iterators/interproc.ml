0e1eec42e2e96cd0e7573bf3d06957168c8f1a77
Abdelraouf Ouadjaout <ouadjaout@gmail.com>
2022-11-26 14:38:07 +0100

    [c] interproc: rename variables of stubs in nested calls

--- analyzer/src/mopsa_analyzer/lang/c/iterators/interproc.ml
+++ analyzer/src/mopsa_analyzer/lang/c/iterators/interproc.ml
@@ -72,6 +72,7 @@ struct
 
   let init prog man flow = set_env T_cur empty man flow
 
+
   (************************)
   (** Auxiliary variables *)
   (************************)
@@ -138,14 +139,14 @@ struct
     Universal.Iterators.Interproc.Common.check_recursion f_orig f_uniq range
     (Callstack.push_callstack f_orig ~uniq:f_uniq range (Flow.get_callstack flow))
 
+  let is_local_scope = function
+    | Variable_local _ | Variable_parameter _ ->
+      true
+    | _ ->
+      false
+
   (** Rename the variables of a function's body by attaching the callstack to them *)
-  let rename_function_variables_body cs body =
-    let is_local_scope = function
-      | Variable_local _ | Variable_parameter _ ->
-        true
-      | _ ->
-        false
-    in
+  let rename_variables_in_function_body cs body =
     let rec visit_expr e =
       map_expr
         (fun e ->
@@ -184,6 +185,32 @@ struct
              let vars' = List.map (mk_stack_var cs) vars in
              let s' = { s with skind = S_block(block', vars') } in
              Keep s'
+           | S_c_return(e, upd) ->
+             let e' = OptionExt.lift visit_expr e in
+             let upd' = visit_scope_update upd in
+             let s' = { s with skind = S_c_return(e', upd') } in
+             Keep s'
+           | S_c_break upd ->
+             let upd' = visit_scope_update upd in
+             let s' = { s with skind = S_c_break upd' } in
+             Keep s'
+           | S_c_continue upd ->
+             let upd' = visit_scope_update upd in
+             let s' = { s with skind = S_c_continue upd' } in
+             Keep s'
+           | S_c_goto(label, upd) ->
+             let upd' = visit_scope_update upd in
+             let s' = { s with skind = S_c_goto(label, upd') } in
+             Keep s'
+           | S_c_switch_case(e, upd) ->
+             let e' = visit_expr e in
+             let upd' = visit_scope_update upd in
+             let s' = { s with skind = S_c_switch_case(e', upd') } in
+             Keep s'
+           | S_c_switch_default upd ->
+             let upd' = visit_scope_update upd in
+             let s' = { s with skind = S_c_switch_default upd' } in
+             Keep s'
            | _ ->
              VisitParts s
         ) s
@@ -194,23 +221,99 @@ struct
         C_init_list(List.map visit_init inits, OptionExt.lift visit_init filler)
       | C_init_implicit t ->
         C_init_implicit t
+    and visit_scope_update upd =
+      { c_scope_var_added = List.map (mk_stack_var cs) upd.c_scope_var_added;
+        c_scope_var_removed = List.map (mk_stack_var cs) upd.c_scope_var_removed; }
     in
     visit_stmt body
 
   (** Renamve local variables in a stub body by attaching the callstack to them *)
-  let rename_function_variables_stub cs stub =
-    assert false
+  let rename_variables_in_stub cs stub =
+    let with_range f x =
+      bind_range x f
+    in
+    let rec visit_section = function
+      | S_case case -> S_case (visit_case case)
+      | S_leaf leaf -> S_leaf (visit_leaf leaf)
+    and visit_case case =
+      { case with
+        case_body    = List.map visit_leaf case.case_body;
+        case_locals  = List.map (with_range visit_local) case.case_locals;
+        case_assigns = List.map (with_range visit_assigns) case.case_assigns; }
+    and visit_leaf = function
+      | S_local local       -> S_local (with_range visit_local local)
+      | S_assumes assumes   -> S_assumes (with_range visit_formula assumes)
+      | S_requires requires -> S_requires (with_range visit_formula requires)
+      | S_assigns assigns   -> S_assigns (with_range visit_assigns assigns)
+      | S_ensures ensures   -> S_ensures (with_range visit_formula ensures)
+      | S_free free         -> S_free (with_range visit_expr free)
+      | S_message _ as x    -> x
+    and visit_local local =
+      { lvar = mk_stack_var cs local.lvar;
+        lval = visit_local_value local.lval; }
+    and visit_local_value = function
+      | L_new _ as x -> x
+      | L_call(f, args) ->
+        L_call(visit_expr f, List.map visit_expr args)
+    and visit_assigns assigns =
+      { assign_target = visit_expr assigns.assign_target;
+        assign_offset = List.map visit_interval assigns.assign_offset; }
+    and visit_interval (lo, hi) =
+      (visit_expr lo, visit_expr hi)
+    and visit_expr e =
+      map_expr
+        (fun e ->
+           match ekind e with
+           | E_var(v, mode) ->
+             begin match vkind v with
+               | V_cvar cv when is_local_scope cv.cvar_scope ->
+                 let v' = mk_stack_var cs v in
+                 let e' = { e with ekind = E_var(v', mode) } in
+                 Keep e'
+               | _ ->
+                 Keep e
+             end
+           | _ ->
+             VisitParts e
+        )
+        (fun s -> VisitParts s)
+        e
+    and visit_formula f =
+      with_range (function
+          | F_expr e            -> F_expr (visit_expr e)
+          | F_binop(op, f1, f2) -> F_binop(op, visit_formula f1, visit_formula f2)
+          | F_not f             -> F_not (visit_formula f)
+          | F_forall(v, s, f)   -> F_forall(mk_stack_var cs v, visit_set s, visit_formula f)
+          | F_exists(v, s, f)   -> F_exists(mk_stack_var cs v, visit_set s, visit_formula f)
+          | F_in(e, s)          -> F_in(visit_expr e, visit_set s)
+          | F_otherwise(f, e)   -> F_otherwise(visit_formula f, visit_expr e)
+          | F_if(f1, f2, f3)    -> F_if(visit_formula f1, visit_formula f2, visit_formula f3)
+        ) f
+    and visit_set = function
+      | S_interval i      -> S_interval (visit_interval i)
+      | S_resource _ as x -> x
+    in
+    { stub with
+      stub_func_body = List.map visit_section stub.stub_func_body;
+      stub_func_params = List.map (mk_stack_var cs) stub.stub_func_params;
+      stub_func_locals = List.map (with_range visit_local) stub.stub_func_locals;
+      stub_func_assigns = List.map (with_range visit_assigns) stub.stub_func_assigns; }
 
   (** Rename the variables of a function by attaching the callstack to them *)
-  let rename_function_variables cs fundec =
-    if is_empty_callstack cs then
+  let rename_variables_in_fundec cs fundec =
+    (* No renaming the function is not in the callstack *)
+    let first_call =
+      cs |> List.for_all
+        (fun c -> c.call_fun_uniq_name = fundec.c_func_unique_name)
+    in
+    if first_call then
       fundec
     else
       { fundec with
         c_func_parameters = List.map (mk_stack_var cs) fundec.c_func_parameters;
         c_func_local_vars = List.map (mk_stack_var cs) fundec.c_func_local_vars;
-        c_func_body = OptionExt.lift (rename_function_variables_body cs) fundec.c_func_body;
-        c_func_stub = OptionExt.lift (rename_function_variables_stub cs) fundec.c_func_stub;
+        c_func_body = OptionExt.lift (rename_variables_in_function_body cs) fundec.c_func_body;
+        c_func_stub = OptionExt.lift (rename_variables_in_stub cs) fundec.c_func_stub;
       }
 
   (** 𝔼⟦ alloca(size) ⟧ *)
@@ -286,7 +389,7 @@ struct
         )
         else
          let () = debug "not a recursive call" in
-         let fundec = rename_function_variables (Flow.get_callstack flow) fundec in
+         let fundec = rename_variables_in_fundec (Flow.get_callstack flow) fundec in
          match fundec with
          | {c_func_body = Some body; c_func_stub = None; c_func_variadic = false} ->
            debug "body = %a" pp_stmt body;
@@ -381,9 +484,6 @@ struct
 
   let eval exp man flow =
     match ekind exp with
-    | E_call({ ekind = E_c_function { c_func_variadic = true}}, args) ->
-      None
-
     | E_call({ ekind = E_c_function f}, args) ->
       eval_call f args exp.erange man flow |>
       OptionExt.return
