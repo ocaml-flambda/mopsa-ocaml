8282018172141df204158df131412ac636ff3c0d
sspies <sspies@janestreet.com>
2023-08-22 13:42:38 +0100

    fix initialization

--- analyzer/languages/c/iterators/program.ml
+++ analyzer/languages/c/iterators/program.ml
@@ -610,11 +610,12 @@ struct
       let call_function (f: c_fundec) = 
         let value_typ = (T_c_integer (C_signed_long)) in
         let tmp_active_var = mktmp ~typ:value_typ () in 
-        let dec_var = mk_c_declaration tmp_active_var (Some (C_init_expr (mk_top value_typ f.c_func_name_range))) Variable_global f.c_func_name_range in
-        let active_var = mk_expr_stmt (mk_ffi_call "_ffi_mark_active_value" [mk_var tmp_active_var f.c_func_name_range] f.c_func_name_range) f.c_func_name_range in
-        let args = List.map (fun arg -> mk_var tmp_active_var f.c_func_name_range) (f.c_func_parameters) in 
+        let tmp_active = mk_var tmp_active_var f.c_func_name_range in 
+        let declare_var = mk_c_declaration tmp_active_var (Some (C_init_expr (mk_top value_typ f.c_func_name_range))) Variable_global f.c_func_name_range in
+        let active_var = mk_expr_stmt (mk_ffi_call "_ffi_mark_active_contents" [mk_c_address_of tmp_active f.c_func_name_range] f.c_func_name_range) f.c_func_name_range in
+        let args = List.map (fun arg -> tmp_active) (f.c_func_parameters) in 
         let exec_fun = mk_c_call_stmt f args f.c_func_name_range in 
-        mk_block [dec_var; active_var; exec_fun] f.c_func_name_range
+        mk_block [declare_var; active_var; exec_fun] f.c_func_name_range
       in
       let rec exec_all_tests (fs: c_fundec list) (flows: 'a flow list) (flow: 'a flow) =
         match fs with 
