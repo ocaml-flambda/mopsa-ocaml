6dd2ea952070aa23d5f990cf9dcc157921125633
sspies <sspies@janestreet.com>
2023-09-19 13:47:15 +0100

    more comments

--- analyzer/languages/c/memory/runtime/value.ml
+++ analyzer/languages/c/memory/runtime/value.ml
@@ -1,26 +1,4 @@
-(****************************************************************************)
-(*                                                                          *)
-(* This file is part of MOPSA, a Modular Open Platform for Static Analysis. *)
-(*                                                                          *)
-(* Copyright (C) 2017-2019 The MOPSA Project.                               *)
-(*                                                                          *)
-(* This program is free software: you can redistribute it and/or modify     *)
-(* it under the terms of the GNU Lesser General Public License as published *)
-(* by the Free Software Foundation, either version 3 of the License, or     *)
-(* (at your option) any later version.                                      *)
-(*                                                                          *)
-(* This program is distributed in the hope that it will be useful,          *)
-(* but WITHOUT ANY WARRANTY; without even the implied warranty of           *)
-(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *)
-(* GNU Lesser General Public License for more details.                      *)
-(*                                                                          *)
-(* You should have received a copy of the GNU Lesser General Public License *)
-(* along with this program.  If not, see <http://www.gnu.org/licenses/>.    *)
-(*                                                                          *)
-(****************************************************************************)
-
-
-(** Non-relational abstraction of pointer values *)
+(** Auxillary definitions for OCaml runtime check *)
 
 
 open Mopsa
@@ -36,44 +14,41 @@ open Framework.Lattices.Partial_inversible_map
 
 module Roots =
 struct
-  type t = 
+  type t =
     Rooted | NotRooted
 
   let compare a b =
-    match a, b with 
+    match a, b with
     | NotRooted, NotRooted -> 0
     | Rooted, Rooted -> 0
     | _, _ -> Stdlib.compare a b
 
 
-  let to_string s = 
+  let to_string s =
     match s with
     | Rooted-> "root"
     | NotRooted -> "not rooted"
-    
+
   let print printer b = pp_string printer (to_string b)
 end
 
 
 module RuntimeLock =
 struct
-  type t = 
-    Locked | Unlocked 
+  type t =
+    Locked | Unlocked
 
   let compare a b =
-    match a, b with 
+    match a, b with
     | Locked, Locked -> 0
     | Unlocked, Unlocked -> 0
     | _, _ -> Stdlib.compare a b
 
 
-  let to_string s = 
+  let to_string s =
     match s with
     | Locked -> "locked"
     | Unlocked-> "unlocked"
-    
+
   let print printer b = pp_string printer (to_string b)
 end
-
-
-
