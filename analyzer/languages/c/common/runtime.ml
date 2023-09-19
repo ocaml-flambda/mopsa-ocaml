45dc51813dd6d686afdd01cfb9477a7a8b7eccd2
sspies <sspies@janestreet.com>
2023-09-19 13:49:53 +0100

    move structures

--- analyzer/languages/c/common/runtime.ml
+++ analyzer/languages/c/common/runtime.ml
@@ -49,6 +49,47 @@ struct
   let print printer b = pp_string printer (to_string b)
 end
 
+(* Runtime Roots *)
+module Roots =
+struct
+  type t =
+    Rooted | NotRooted
+
+  let compare a b =
+    match a, b with
+    | NotRooted, NotRooted -> 0
+    | Rooted, Rooted -> 0
+    | _, _ -> Stdlib.compare a b
+
+
+  let to_string s =
+    match s with
+    | Rooted-> "root"
+    | NotRooted -> "not rooted"
+
+  let print printer b = pp_string printer (to_string b)
+end
+
+(* Runtime Lock *)
+module RuntimeLock =
+struct
+  type t =
+    Locked | Unlocked
+
+  let compare a b =
+    match a, b with
+    | Locked, Locked -> 0
+    | Unlocked, Unlocked -> 0
+    | _, _ -> Stdlib.compare a b
+
+
+  let to_string s =
+    match s with
+    | Locked -> "locked"
+    | Unlocked-> "unlocked"
+
+  let print printer b = pp_string printer (to_string b)
+end
 
 
 
